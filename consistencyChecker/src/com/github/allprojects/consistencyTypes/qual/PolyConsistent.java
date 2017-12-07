2bb30a3c7c64140718619b56b208b90116d2103b
Jesper Schlegel <jesperhschlegel@gmail.com>
2017-12-07 15:35:49 +0100

    + PolyConsistent + SuppressWarningKey

---
+++ consistencyChecker/src/com/github/allprojects/consistencyTypes/qual/PolyConsistent.java
@@ -0,0 +1,21 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+package com.github.allprojects.consistencyTypes.qual;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.checkerframework.framework.qual.PolymorphicQualifier;
+
+@Documented
+@PolymorphicQualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface PolyConsistent {
+}
