502604892adb886b93cb009a3fd0b55ad96308b9
Jesper Schlegel <jesperhschlegel@gmail.com>
2017-12-17 16:41:49 +0100

    ConsistencyChecker: do not use flow analysis

--- consistencyCheckerTest/src/Test.java
+++ consistencyCheckerTest/src/Test.java
@@ -2,17 +2,11 @@ import com.github.allprojects.consistencyTypes.qual.*;
 
 public class Test {
 
-    @Low private int getLowValue(){
-        return 1;
-    }
-
-    @High private int getHighValue(){
-        return 1;
-    }
-
     void shouldFail() {
-        @High int a = getHighValue();
-        int b = getLowValue();
-        @High int c = a + b;
+        @High int h1 = 0;
+        @High int h2 = 1;
+        @Low int l1 = 0;
+        l1 = h1;
+        h2 = l1;
     }
 }
