d06fa059acb01f383b8491cca0475d1414a571a2
Victor Schümmer <victor_ferdinand.schuemmer@stud.tu-darmstadt.de>
2018-01-17 00:34:48 +0100

    more testcases

--- consistencyCheckerTest/consistency/ConsistencyTests.java
+++ consistencyCheckerTest/consistency/ConsistencyTests.java
@@ -15,6 +15,15 @@ class ConsistencyTests {
         a = b;
     }
 
+    void unannotated() {
+        @Low int a;
+        @High int b;
+        int c = 42;
+        a = c;
+        // :: error: (assignment.type.incompatible)
+        b = c;
+    }
+
     void literalAssignment() {
         @Low int a = 42;
         @High int b = 42;
@@ -25,6 +34,7 @@ class ConsistencyTests {
 
     void wantsHigh(@High int a) { }
     void wantsLow(@Low int a) { }
+    void wantsAnything(int a) { }
 
     void methodArguments() {
         @High int high = 42;
@@ -36,6 +46,64 @@ class ConsistencyTests {
 
         wantsLow(high);
         wantsLow(low);
+
+        wantsAnything(high);
+        wantsAnything(low);
+    }
+
+    @Low int getLow() { return 42; }
+    @High int getHigh() { return 42; }
+    int getAnything() { return 42; }
+
+    void methodAnnotation() {
+        @Low int low = getLow();
+        low = getHigh();
+        low = getAnything();
+
+        @High int high = getHigh();
+        // :: error: (assignment.type.incompatible)
+        high = getLow();
+        // :: error: (assignment.type.incompatible)
+        high = getAnything();
+    }
+
+    @Low int lowMethodLowReturn() { @Low int low = 42; return low; }
+    @Low int lowMethodHighReturn() { @High int high = 42; return high; }
+
+    // :: error: (return.type.incompatible)
+    @High int highMethodLowReturn() { @Low int low = 42; return low; }
+    @High int highMethodHighReturn() { @High int high = 42; return high; }
+
+    int returnPassedLow(@Low int low) { return low; }
+    int returnPassedHigh(@High int high) { return high; }
+    int returnPassedAnything(int anything) { return anything; }
+
+    void valueThroughMethod() {
+        @High int high = 42;
+        @Low int low = 42;
+
+        @High int a = returnPassedHigh(high);
+        a = returnPassedAnything(high);
+        // :: error: (assignment.type.incompatible)
+        a = returnPassedAnything(low);
+        // :: error: (assignment.type.incompatible)
+        a = returnPassedLow(low);
+
+        @Low int b = returnPassedHigh(high);
+        b = returnPassedAnything(high);
+        b = returnPassedAnything(low);
+        b = returnPassedLow(low);
+    }
+
+    @High int sanitize(int anything) {
+        @SuppressWarnings("consistency")
+        @High int sanitized = anything;
+        return sanitized;
+    }
+
+    void testSanitize() {
+        @Low int low = 42;
+        @High int high = sanitize(low);
     }
 
 }
