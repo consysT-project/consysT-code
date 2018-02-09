88e1fb6c3d4cbc76ad9d077b701b0e92ffff91fa
Jesper Schlegel <jesperhschlegel@gmail.com>
2018-02-09 11:21:17 +0100

    ReturnArgument: PolyConsistent Qualifier

--- consistencyCheckerTest/consistency/ReturnArgument.java
+++ consistencyCheckerTest/consistency/ReturnArgument.java
@@ -2,25 +2,17 @@ import com.github.allprojects.consistencyTypes.qual.*;
 
 class ReturnArgument {
 
-    int returnPassedLow(@Low int low) { return low; }
-    int returnPassedHigh(@High int high) { return high; }
-    int returnPassedAnything(int anything) { return anything; }
+    @PolyConsistent int identity( @PolyConsistent int number) {return number; }
 
-    void valueThroughMethod() {
+    void returnArgument() {
         @High int high = 42;
         @Low int low = 42;
 
-        @High int a = returnPassedHigh(high);
-        a = returnPassedAnything(high);
+        @High int a = identity(high);
         // :: error: (assignment.type.incompatible)
-        a = returnPassedAnything(low);
-        // :: error: (assignment.type.incompatible)
-        a = returnPassedLow(low);
+        a = identity(low);
 
-        @Low int b = returnPassedHigh(high);
-        b = returnPassedAnything(high);
-        b = returnPassedAnything(low);
-        b = returnPassedLow(low);
+        @Low int b = identity(high);
+        b = identity(low);
     }
-
 }
