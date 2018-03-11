90fc7cb0f2ae928fdf24d6311f5657e972e9b0bc
Jesper Schlegel <jesperhschlegel@gmail.com>
2018-03-11 23:55:44 +0100

    cassandra example application - WIP

---
+++ consistentCassandra/src/main/java/Customer.java
@@ -0,0 +1,19 @@
+public class Customer {
+
+    static int id_count = 0;
+
+    public String name;
+    public int amount;
+    public int id;
+
+    public static int getNewID(){
+        id_count++;
+        return id_count;
+    }
+
+    public Customer(String n, int a){
+        this.id = Customer.getNewID();
+        this.name = n;
+        this.amount = a;
+    }
+}
