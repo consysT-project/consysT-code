021a07d9b7f33c49ab8f35ada4793a7d4afe22f0
Victor Schümmer <victor_ferdinand.schuemmer@stud.tu-darmstadt.de>
2018-03-20 16:23:57 +0100

    extended cassandra example application (WIP)

--- consistentCassandra/src/main/java/BankConnector.java
+++ consistentCassandra/src/main/java/BankConnector.java
@@ -3,9 +3,11 @@ import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.github.allprojects.consistencyTypes.qual.High;
 import com.github.allprojects.consistencyTypes.qual.Low;
 
-public class BankConnector {
-    private Cluster cluster;
-    private Session session;
+import javax.swing.plaf.nimbus.State;
+
+import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
+
+public class BankConnector extends ConsistentCassandraConnector {
 
     private final String customerTableName = "customers";
     private final String idKey = "id";
@@ -16,64 +18,24 @@ public class BankConnector {
 
     }
 
-    public void connect(String node, Integer port) {
-        Cluster.Builder b = Cluster.builder().addContactPoint(node);
-        if (port != null) {
-            b.withPort(port);
-        }
-        cluster = b.build();
-        session = cluster.connect();
-    }
-
-
-    @High
-    public ResultSet executeAll(@High Statement statement){
-        statement.setConsistencyLevel(ConsistencyLevel.ALL);
-        @SuppressWarnings("consistency")
-        @High ResultSet result = session.execute(statement);
-        return result;
-    }
-
-    @Low
-    public ResultSet executeSingle(@Low Statement statement){
-        statement.setConsistencyLevel(ConsistencyLevel.ONE);
-        return session.execute(statement);
-    }
-
-    public Session getSession() {
-        return this.session;
-    }
-
-    public void close() {
-        session.close();
-        cluster.close();
-    }
-
-    public void createKeyspace(String keyspaceName, String replicationStrategy, int replicationFactor) {
-        StringBuilder sb =
-                new StringBuilder("CREATE KEYSPACE IF NOT EXISTS ")
-                        .append(keyspaceName).append(" WITH replication = {")
-                        .append("'class':'").append(replicationStrategy)
-                        .append("','replication_factor':").append(replicationFactor)
-                        .append("};");
-
-        String query = sb.toString();
-        session.execute(query);
-        session = cluster.connect(keyspaceName);
-    }
-
-    public void dropKeyspace(String name){
-        String query = "DROP KEYSPACE " + name;
-        session.execute(query);
-    }
-
     public void createCustomerTable(){
-        session.execute("CREATE TABLE " + customerTableName + " ("+ idKey +" int primary key, "+ nameKey + " varchar, "+ amountKey +" int);");
+        getSession().execute("CREATE TABLE " + customerTableName + " ("+ idKey +" int primary key, "+ nameKey + " varchar, "+ amountKey +" int);");
     }
 
     public void addCustomer(Customer c){
-        String query = QueryBuilder.insertInto(customerTableName).values(new String[]{idKey, nameKey, amountKey}, new Object[]{c.id, c.name, c.amount}).getQueryString();
-        SimpleStatement statement = new SimpleStatement(query);
-        this.executeAll(statement);
+        @High Statement query = QueryBuilder.insertInto(customerTableName).values(new String[] { idKey, nameKey, amountKey }, new Object[] { c.id, c.name, c.amount });
+        this.executeAll(query);
+    }
+
+    public void withdraw(Customer c, int amount){
+        @High Statement query = QueryBuilder.select().from(customerTableName).where(eq(idKey, c.id));
+        @High int balance = this.executeAll(query).one().getInt(amountKey);
+        query = QueryBuilder.update(customerTableName).where(eq(idKey, c.id)).with(QueryBuilder.set(amountKey, balance - amount));
+        this.executeAll(query);
+    }
+
+    public int getBalance(Customer c){
+        @Low Statement query = QueryBuilder.select().from(customerTableName).where(eq(idKey, c.id));
+        return this.executeSingle(query).one().getInt(amountKey);
     }
 }
---
+++ consistentCassandra/src/test/java/BankConnectorTest.java
@@ -0,0 +1,51 @@
+import com.github.allprojects.consistencyTypes.qual.High;
+import com.github.allprojects.consistencyTypes.qual.Low;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BankConnectorTest {
+
+    private Integer port = 9042;
+    private String node = "localhost";
+    private Bank bank;
+
+    @Before
+    public void setUp() {
+        BankConnector connector = new BankConnector();
+        connector.connect(node, port);
+        bank = new Bank(connector);
+    }
+
+    @Test
+    public void simpleTest() {
+        Customer c = new Customer("Peter");
+        bank.addCustomer(c);
+        assert bank.getBalance(c) == 0;
+    }
+
+    @Low public int someInconsistentCalculation() { return 42; }
+
+    @Test
+    public void withdrawalTest() {
+        @High int amountA = 1000;
+        @Low int amountB = someInconsistentCalculation();
+        int amountC = someInconsistentCalculation();
+
+        Customer c = new Customer("Peter");
+        bank.addCustomer(c);
+        bank.withdraw(c, amountA);
+        assert bank.getBalance(c) == -1000;
+        bank.withdraw(c, -amountA);
+        assert bank.getBalance(c) == 0;
+        // :: error: (argument.type.incompatible)
+        bank.withdraw(c, amountB);
+        // :: error: (argument.type.incompatible)
+        bank.withdraw(c, amountC);
+    }
+
+    @After
+    public void tearDown(){
+        bank.close();
+    }
+}
