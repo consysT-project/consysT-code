a21eca8c62d38f91e6fd3a77c52c1bd1e7e04b9d
Mirko Köhler <kohler.mirko@gmail.com>
2018-05-22 18:16:39 +0200

    improved qualifier lattice

--- consistency-checker/src/main/java/de/tu_darmstadt/consistency_types/checker/qual/None.java
+++ consistency-checker/src/main/java/de/tu_darmstadt/consistency_types/checker/qual/None.java
@@ -1,13 +1,15 @@
 package de.tu_darmstadt.consistency_types.checker.qual;
 
+import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
+import org.checkerframework.framework.qual.SubtypeOf;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.checkerframework.framework.qual.*;
-
-@SubtypeOf({Weak.class})
+@SubtypeOf({})
 @Target({ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface Strong {}
+@DefaultQualifierInHierarchy
+public @interface None {}
