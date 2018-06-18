b37634b5d4c1a039dfe0a2d4c3c23d667aba7fd9
Mirko Köhler <kohler.mirko@gmail.com>
2018-06-18 18:07:13 +0200

    Added a richer test environment

---
+++ consistency-store/src/test/java/de/tudarmstadt/consistency/store/cassandra/CassandraStoreTest.java
@@ -0,0 +1,9 @@
+package de.tudarmstadt.consistency.store.cassandra;
+
+/**
+ * Created on 18.06.18.
+ *
+ * @author Mirko Köhler
+ */
+public class CassandraStoreTest {
+}
---
+++ consistency-store/src/test/java/de/tudarmstadt/consistency/store/data/A.java
@@ -0,0 +1,30 @@
+package de.tudarmstadt.consistency.demo.data;
+
+import de.tudarmstadt.consistency.checker.qual.Strong;
+import de.tudarmstadt.consistency.store.Handle;
+import de.tudarmstadt.consistency.store.StateEvent;
+
+import java.io.Serializable;
+
+/**
+ * Created on 12.06.18.
+ *
+ * @author Mirko Köhler
+ */
+/* Restrict to record syntax. */
+public class A implements Serializable {
+	private final int x;
+	public final Handle<? extends B, StateEvent> b;
+	private final String z;
+
+	//? extends B is needed as we are not getting a B but, e.g., @Strong B.
+	public A(int x, Handle<? extends B, StateEvent> b, String z) {
+		this.x = x;
+		this.b = b;
+		this.z = z;
+	}
+
+	public String toString() {
+		return String.format("A(x=%s, b=%s, z=%s)", x, b, z);
+	}
+}
