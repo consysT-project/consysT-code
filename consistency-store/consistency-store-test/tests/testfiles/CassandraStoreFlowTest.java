284e6b48877f00f5b3a6ae111d1ccac1f6c56e13
Mirko Köhler <kohler.mirko@gmail.com>
2018-06-18 18:07:13 +0200

    Added a richer test environment

--- consistency-store/consistency-store-test/tests/testfiles/CassandraStoreFlowTest.java
+++ consistency-store/consistency-store-test/tests/testfiles/CassandraStoreFlowTest.java
@@ -1,3 +1,4 @@
+import de.tudarmstadt.consistency.checker.qual.Local;
 import de.tudarmstadt.consistency.checker.qual.Strong;
 import de.tudarmstadt.consistency.checker.qual.Weak;
 import de.tudarmstadt.consistency.store.Handle;
@@ -17,19 +18,16 @@ class CassandraStoreFlowTest {
 	/* Restrict to record syntax. */
 	public static class A implements Serializable {
 		final int x;
-		final Handle<? extends B, StateEvent> b;
+		final CassandraHandle<? extends B> b;
 		final String z;
 
 		//? extends B is needed as we are not getting a B but, e.g., @Strong B.
-		A(int x, Handle<? extends B, StateEvent> b, String z) {
+		A(int x, CassandraHandle<? extends B> b, String z) {
 			this.x = x;
 			this.b = b;
 			this.z = z;
 		}
 
-		public String toString() {
-			return String.format("A(x=%s, b=%s, z=%s)", x, b, z);
-		}
 	}
 
 	public static class B implements Serializable {
@@ -38,40 +36,21 @@ class CassandraStoreFlowTest {
 		B(String s) {
 			this.s = s;
 		}
-
-		public String toString() {
-			return "B(s=" + s + ")";
-		}
 	}
 
-	public static class O implements Serializable {
-		final A a;
-		final String s;
-
-		O(A a, String s) {
-			this.a = a;
-			this.s = s;
-		}
-
-		@Override
-		public String toString() {
-			return "O(A=" + a + ", s=" + s + ")";
-		}
-	}
 
 	final UUID idA1 = new UUID(573489594L, 8675789563L);
 	final UUID idA2 = new UUID(573489456L, 1675789879L);
 
 	final UUID idB1 = new UUID(573489512L, 1675789528L);
-	final UUID idB2 = new UUID(573489465L, 1675789841L);
 
 	public void testSetStrong() throws Exception {
 		try (CassandraDatabase database = CassandraDatabase.local()) {
 			CassandraHandle<@Strong A> strongA1 = (CassandraHandle<@Strong A>) database.obtain(idA1, A.class, Strong.class);
 			CassandraHandle<@Strong B> strongB1 = (CassandraHandle<@Strong B>) database.obtain(idB1, B.class, Strong.class);
 
-			strongA1.set(new @Strong A(312, strongB1, "hallo"));
-			strongB1.set(new @Strong B(strongA1.get().z));
+			strongA1.set(new @Local A(312, strongB1, "hallo"));
+			strongB1.set(new @Local B(strongA1.get().z));
 		}
 	}
 
@@ -80,8 +59,8 @@ class CassandraStoreFlowTest {
 			CassandraHandle<@Weak A> weakA1 = (CassandraHandle<@Weak A>) database.obtain(idA1, A.class, Weak.class);
 			CassandraHandle<@Weak B> weakB1 = (CassandraHandle<@Weak B>) database.obtain(idB1, B.class, Weak.class);
 
-			weakA1.set(new @Weak A(312, weakB1, "hallo"));
-			weakB1.set(new @Weak B(weakA1.get().z));
+			weakA1.set(new @Local A(312, weakB1, "hallo"));
+			weakB1.set(new @Local B(weakA1.get().z));
 		}
 	}
 
@@ -122,12 +101,12 @@ class CassandraStoreFlowTest {
 			CassandraHandle<@Strong A> strongA = (CassandraHandle<@Strong A>) database.obtain(idA1, A.class, Strong.class);
 			CassandraHandle<@Weak B> weakB = (CassandraHandle<@Weak B>) database.obtain(idB1, B.class, Weak.class);
 
-			A a = new @Strong A(213, weakB,"fire");
+			A a = new @Local A(213, weakB,"fire");
 
 			if (weakB.get() == null) {
-				// :: error: (invocation.receiver.implicitflow) :: error: (invocation.argument.implicitflow)
+				// :: error: (invocation.receiver.implicitflow)
 				strongA.set(a);
-				// :: error: (invocation.receiver.implicitflow) :: error: (invocation.argument.implicitflow)
+				// :: error: (invocation.receiver.implicitflow)
 				strongA.handle(WRITE, a);
 			}
 		}
@@ -138,15 +117,13 @@ class CassandraStoreFlowTest {
 			CassandraHandle<@Strong A> strongA = (CassandraHandle<@Strong A>) database.obtain(idA1, A.class, Strong.class);
 			CassandraHandle<@Weak B> weakB = (CassandraHandle<@Weak B>) database.obtain(idB1, B.class, Weak.class);
 
-			A a = new @Strong A(213, weakB,"fire");
 
 			B b = weakB.get();
-
 			if (b.s.length() > 3) {
 				// :: error: (invocation.receiver.implicitflow) :: error: (invocation.argument.implicitflow)
-				strongA.set(a);
+				strongA.set(strongA.get());
 				// :: error: (invocation.receiver.implicitflow) :: error: (invocation.argument.implicitflow)
-				strongA.handle(WRITE, a);
+				strongA.handle(WRITE, strongA.get());
 			}
 		}
 	}
@@ -156,7 +133,7 @@ class CassandraStoreFlowTest {
 			CassandraHandle<@Strong A> strongA = (CassandraHandle<@Strong A>) database.obtain(idA1, A.class, Strong.class);
 			CassandraHandle<@Weak B> weakB = (CassandraHandle<@Weak B>) database.obtain(idB1, B.class, Weak.class);
 
-			B b = new @Weak B("hallo");
+			B b = new @Local B("hallo");
 
 			if (strongA.get() == null) {
 				weakB.set(b);
---
+++ consistency-store/src/test/java/de/tudarmstadt/consistency/store/data/B.java
@@ -0,0 +1,22 @@
+package de.tudarmstadt.consistency.demo.data;
+
+import de.tudarmstadt.consistency.checker.qual.Strong;
+
+import java.io.Serializable;
+
+/**
+ * Created on 12.06.18.
+ *
+ * @author Mirko Köhler
+ */
+public class B implements Serializable {
+	private final String s;
+
+	public B(String s) {
+		this.s = s;
+	}
+
+	public String toString() {
+		return "B(s=" + s + ")";
+	}
+}
