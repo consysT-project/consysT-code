b598e1b72f6101aa8884cc98f4838ea4c560107b
Pascal Weisenburger <pascal.weisenburger@web.de>
2019-03-29 13:49:12 +0100

    microbenchmarks

---
+++ microbenchmarks/src/main/java/de/tudarmstadt/consistency/microbenchmarks/BenchmarkObject1.java
@@ -0,0 +1,11 @@
+package de.tudarmstadt.consistency.microbenchmarks;
+
+import de.tudarmstadt.consistency.replobj.java.JRef;
+
+public class BenchmarkObject1 implements BenchmarkObject {
+    public JRef<BenchmarkObject> ref0;
+
+    public BenchmarkObject1(JRef<BenchmarkObject> ref0) {
+        this.ref0 = ref0;
+    }
+}
---
+++ microbenchmarks/src/main/java/de/tudarmstadt/consistency/microbenchmarks/BenchmarkObject2.java
@@ -0,0 +1,15 @@
+package de.tudarmstadt.consistency.microbenchmarks;
+
+import de.tudarmstadt.consistency.replobj.java.JRef;
+
+public class BenchmarkObject2 implements BenchmarkObject {
+    public JRef<BenchmarkObject> ref0;
+    public JRef<BenchmarkObject> ref1;
+
+    public BenchmarkObject2(
+            JRef<BenchmarkObject> ref0,
+            JRef<BenchmarkObject> ref1) {
+        this.ref0 = ref0;
+        this.ref1 = ref1;
+    }
+}
