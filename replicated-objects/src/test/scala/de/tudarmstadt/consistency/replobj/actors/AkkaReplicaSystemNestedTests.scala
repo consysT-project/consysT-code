f55a21f497119fe6ebaea2613baf851feb072039
Mirko Köhler <kohler.mirko@gmail.com>
2019-04-16 13:18:04 +0200

    fixed a bug with caching

--- replicated-objects/src/test/scala/de/tudarmstadt/consistency/replobj/actors/AkkaReplicaSystemNestedTests.scala
+++ replicated-objects/src/test/scala/de/tudarmstadt/consistency/replobj/actors/AkkaReplicaSystemNestedTests.scala
@@ -11,7 +11,7 @@ import org.scalatest.fixture
 	* @author Mirko Köhler
 	*/
 class AkkaReplicaSystemNestedTests extends fixture.FunSuite with AkkaReplicaSystemSuite {
-	override def numOfReplicas : Int = 2
+	override def numOfReplicas : Int = 4
 
 	test("testStrongMasterWithStrongNested") { F =>
 		val replica0 = F(0)
