e8a276b8bc31193d21b56251cc35502317b8b392
Martin Edlund <*lennyface*>
2019-05-14 23:07:04 +0200

    Fixed Overloading error

---
+++ JRefCollections/src/main/java/de/tudarmstadt/consistency/jrefcollections/JRefDistList.java
@@ -0,0 +1,10 @@
+package de.tudarmstadt.consistency.jrefcollections;
+
+import java.io.Serializable;
+
+public class JRefDistList implements Serializable {
+
+    public void test(){
+        System.out.println("Output here");
+    }
+}
--- shopping-cart/src/main/java/de/tudarmstadt/consistency/shoppingcart/Demo.java
+++ shopping-cart/src/main/java/de/tudarmstadt/consistency/shoppingcart/Demo.java
@@ -3,6 +3,8 @@ package de.tudarmstadt.consistency.shoppingcart;
 
 import de.tudarmstadt.consistency.checker.qual.Strong;
 import de.tudarmstadt.consistency.checker.qual.Weak;
+import de.tudarmstadt.consistency.jrefcollections.JRefDistList;
+import de.tudarmstadt.consistency.jrefcollections.JRefLinkedList;
 import de.tudarmstadt.consistency.jrefcollections.JRefLinkedListStrong;
 import de.tudarmstadt.consistency.replobj.japi.JConsistencyLevel;
 import de.tudarmstadt.consistency.replobj.japi.JRef;
@@ -16,7 +18,7 @@ import static de.tudarmstadt.consistency.shoppingcart.Replicas.*;
 public class Demo implements Serializable {
 
     public static void main(String... args) throws Exception {
-        exampleStrongList();
+        exampleList();
     }
 
     public static void example1() throws Exception {
@@ -113,6 +115,46 @@ public class Demo implements Serializable {
         System.out.println("Strong List Ref index 0:" + strongListRef.invoke("get",0));
         System.out.println("Strong List Ref index 1:" + strongListRef.invoke("get",1));
         System.out.println("Strong List Ref count:" + strongListRef.invoke("size"));
+
+        for (JReplicaSystem rep : replicaSystems) {
+            rep.close();
+        }
+    }
+
+    public static void exampleList() throws Exception {
+        JRef<@Strong JRefLinkedList> strongList = replicaSystems[0].replicate("list1", new JRefLinkedList(), JConsistencyLevel.STRONG);
+        JRef<@Strong JRefLinkedList> strongListRef = replicaSystems[1].ref("list1", JRefLinkedList.class, JConsistencyLevel.STRONG);
+
+        JRef<@Strong Item> item1 = replicaSystems[0].replicate("item1", new Item("item1", 5), JConsistencyLevel.STRONG);
+        JRef<@Strong Item> item2 = replicaSystems[0].replicate("item2", new Item("item2", 10), JConsistencyLevel.STRONG);
+        JRef<@Strong Item> item3 = replicaSystems[0].replicate("item3", new Item("item3", 15), JConsistencyLevel.STRONG);
+        JRef<@Strong Item> item4 = replicaSystems[0].replicate("item4", new Item("item3", 20), JConsistencyLevel.STRONG);
+
+
+        strongList.invoke("append", item1);
+        strongList.invoke("append", item2);
+
+        strongListRef.invoke("append", item3);
+        strongListRef.invoke("append", item4);
+
+        System.out.println("Strong List index 0:" + strongList.invoke("get",0));
+        System.out.println("Strong List Ref index 0:" + strongListRef.invoke("get",0));
+        System.out.println("Strong List Ref index 1:" + strongListRef.invoke("get",1));
+        System.out.println("Strong List Ref count:" + strongListRef.invoke("size"));
+        //strongListRef.sync();
+        System.out.println("Strong List Ref index 0:" + strongListRef.invoke("get",0));
+        System.out.println("Strong List Ref index 1:" + strongListRef.invoke("get",1));
+        System.out.println("Strong List Ref count:" + strongListRef.invoke("size"));
+
+        for (JReplicaSystem rep : replicaSystems) {
+            rep.close();
+        }
+    }
+
+    public static void example2() throws Exception {
+        JRef<@Strong JRefLinkedList> replicated = replicaSystems[0].replicate("killme", new JRefLinkedList(), JConsistencyLevel.STRONG);
+
+        replicated.invoke("add");
 
         for (JReplicaSystem rep : replicaSystems) {
             rep.close();
