55cdd1427b0e0b5bcb30d4a8e58baaab51d98ec5
Martin Edlund <*lennyface*>
2019-06-29 10:59:13 +0200

    More Progress on case study

--- case-study/src/main/java/de.tuda.stg.consys.casestudy/Cart.java
+++ case-study/src/main/java/de.tuda.stg.consys.casestudy/Cart.java
@@ -6,7 +6,6 @@ import de.tuda.stg.consys.jrefcollections.JRefDistList;
 import de.tuda.stg.consys.objects.japi.JConsistencyLevel;
 import de.tuda.stg.consys.objects.japi.JRef;
 import de.tuda.stg.consys.objects.japi.JReplicaSystem;
-import org.checkerframework.org.apache.commons.lang3.NotImplementedException;
 
 import java.io.Serializable;
 
@@ -18,23 +17,34 @@ public class Cart implements Serializable {
         cartContent = system.replicate(new JRefDistList(JConsistencyLevel.WEAK), JConsistencyLevel.WEAK);
     }
 
-    //TODO: figure out how to solve the problem that we want to search by item, but items are wrapped in cartElement
-    //       wrapper
-
+    /*
+     * Add an Item to the Cart, if the item is already in the cart, add the new count to the old count,
+     * if the new count is less or equal to zero return false.
+     */
     public boolean add(JRef<@Strong Product> item, int count, JReplicaSystem system) throws Error{
-        JRef<@Weak cartElement> newCartElem = system.replicate(new cartElement(item, count), JConsistencyLevel.WEAK);
-        return cartContent.invoke("append", newCartElem, system);
+        JRef<@Strong cartElement> itemWrapper =  cartContent.invoke("getItem", item, true);
+        if(count <= 0) return false;
+        if(itemWrapper == null){
+            JRef<@Strong cartElement> newWrapper = system.replicate(new cartElement(item, count), JConsistencyLevel.STRONG);
+            cartContent.invoke("append", newWrapper, system);
+        }else {
+            itemWrapper.setField("count",((int) itemWrapper.getField("count") + count));
+        }
+        return true;
     }
 
+    /*
+     * Return an Item.
+     */
     public boolean remove(JRef<@Strong Product> item){
-        return (cartContent.invoke("removeItem", item) != null);
+        return (cartContent.invoke("removeItem", item, true) != null);
     }
 
     public boolean increase(int count, JRef<@Strong Product> item) throws Error{
         if(count < 0)
             throw new IllegalArgumentException("Count must be at least 0");
         else{
-            JRef<@Strong cartElement> element = (JRef) cartContent.invoke("getItem", item);
+            JRef<@Strong cartElement> element = (JRef) cartContent.invoke("getItem", item, true);
             element.setField("count",((int) element.getField("count")  + count));
             return true;
         }
@@ -44,19 +54,28 @@ public class Cart implements Serializable {
         if(count < 0)
             throw new IllegalArgumentException("Count must be at least 0");
         else{
-            JRef<@Strong cartElement> element = (JRef) cartContent.invoke("getItem", item);
+            JRef<@Strong cartElement> element = (JRef) cartContent.invoke("getItem", item, true);
             int currCount = (int) element.getField("count");
             currCount = currCount - count;
             if(!(currCount > 0)){
                 // if the object count is 0, simply remove the object from the cart
                 return remove(item);
-
             }else{
                 element.setField("count",currCount);
                 return true;
             }
-
         }
+    }
+
+    public double checkout(){
+        double cost = 0;
+        int len = cartContent.invoke("size", true);
+        for(int i = 0; i < len; i++){
+            JRef<@Strong cartElement> curr = cartContent.invoke("getIndex", i, true);
+            cost += ((int) curr.getField("count") *
+                    (double)((JRef) curr.getField("item")).invoke("getCost"));
+        }
+        return cost;
     }
 
     public class cartElement{
@@ -69,6 +88,12 @@ public class Cart implements Serializable {
             if(count < 0)
                 throw new IllegalArgumentException("Item count cannot be negative.");
             this.count = count;
+        }
+
+        //Overridden to make searching a list of cart element wrappers possible
+        @Override
+        public boolean equals(Object o){
+            return (item.equals(o));
         }
     }
 }
