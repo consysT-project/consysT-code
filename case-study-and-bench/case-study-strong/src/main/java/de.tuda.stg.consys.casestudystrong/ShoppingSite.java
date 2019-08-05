4960665a0b9a3dd3d6618d247729f14438af6ad8
Martin Edlund (Laptop) <me@fwegmann.com>
2019-08-05 16:34:57 +0200

    More Changes

---
+++ case-study-and-bench/case-study-strong/src/main/java/de.tuda.stg.consys.casestudystrong/ShoppingSite.java
@@ -0,0 +1,187 @@
+package de.tuda.stg.consys.casestudystrong;
+
+import com.sun.tools.javac.util.ArrayUtils;
+import de.tuda.stg.consys.checker.qual.Strong;
+import de.tuda.stg.consys.checker.qual.Weak;
+import de.tuda.stg.consys.objects.actors.AkkaReplicaSystem;
+import de.tuda.stg.consys.objects.japi.JConsistencyLevel;
+import de.tuda.stg.consys.objects.japi.JRef;
+import de.tuda.stg.consys.objects.japi.JReplicaSystem;
+import de.tuda.stg.consys.objects.japi.JReplicated;
+import org.checkerframework.com.google.common.collect.ObjectArrays;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.Optional;
+
+public class ShoppingSite implements Serializable, JReplicated {
+
+    /* This field is needed for JReplicated */
+    public transient AkkaReplicaSystem<String> replicaSystem = null;
+
+    JRef<@Strong User> currentlyLoggedIn;
+
+    JRef<@Strong Cart> CartOfLoggedIn;
+
+    JRef<@Strong Database> Database;
+
+    public LinkedList<JRef<@Strong Product>> FoundProducts;
+
+    public ShoppingSite(JRef<@Strong Database> db) {
+        Database = db;
+    }
+
+    public boolean RegisterNewUser(String UserName, String Password){
+        Optional<JReplicaSystem> systemOptional = getSystem();
+        JReplicaSystem system;
+        if(systemOptional.isPresent())
+            system = systemOptional.get();
+        else
+            return false;
+
+        if(currentlyLoggedIn != null){
+            System.out.println("Cannot register a new user, you are already logged in as ''" +
+                    currentlyLoggedIn.invoke("getName") +"''");
+            return false;
+        }
+        JRef<@Strong User> newUser = system.replicate(new User(UserName, Password), JConsistencyLevel.STRONG);
+        newUser.invoke("init");
+        if(Database.invoke("RegisterUser", UserName, Password, newUser)){
+            return Login(UserName, Password);
+        }
+        return false;
+    }
+
+    public boolean Login(String UserName, String Password){
+        Optional<JReplicaSystem> systemOptional = getSystem();
+        JReplicaSystem system;
+        if(systemOptional.isPresent())
+            system = systemOptional.get();
+        else
+            return false;
+
+        if(currentlyLoggedIn != null){
+            System.out.println("Cannot Log in, you are already logged in as ''" +
+                    currentlyLoggedIn.invoke("getName") +"''");
+            return false;
+        }
+        JRef<@Strong User> user = Database.invoke("GetUser", UserName, Password, system.toString());
+
+        if(user == null){
+            return false;
+        }
+
+        currentlyLoggedIn = user;
+        CartOfLoggedIn = currentlyLoggedIn.invoke("FetchCart", system.toString());
+        return  true;
+    }
+
+    public boolean Logout(){
+        Optional<JReplicaSystem> systemOptional = getSystem();
+        JReplicaSystem system;
+        if(systemOptional.isPresent())
+            system = systemOptional.get();
+        else
+            return false;
+
+
+        if(currentlyLoggedIn == null){
+            System.out.println("Cannot Log out, you are already logged out.");
+            return false;
+        }
+
+        currentlyLoggedIn.invoke("Logout", system.toString());
+        currentlyLoggedIn = null;
+        CartOfLoggedIn = null;
+        return true;
+    }
+
+    public LinkedList<JRef<Product>> Search(String SearchTerm, boolean printResults){
+        FoundProducts = Database.invoke("SearchProducts", SearchTerm);
+
+        if(printResults) {
+            System.out.println("Found Products:");
+            for (int i = 0; i < FoundProducts.size(); i++) {
+                System.out.println((i + 1) + ") " + FoundProducts.get(i).invoke("getName"));
+            }
+        }
+        return FoundProducts;
+    }
+
+
+
+    public boolean FromFoundAddToCart(int number, int count){
+        int index = number - 1;
+        if(index < 0 || index >= FoundProducts.size()){
+            System.out.println("Please select a valid product");
+            return false;
+        }
+        if(CartOfLoggedIn != null){
+            CartOfLoggedIn.invoke("add", FoundProducts.get(index), count);
+            return true;
+        }
+        System.out.println("You are not logged in.");
+        return false;
+    }
+
+    public String FromFoundDisplayInfo(int number, boolean getComments, boolean printInfo){
+        int index = number - 1;
+        String ret;
+        if(index < 0 || index >= FoundProducts.size()) {
+            ret = "Please select a valid product";
+        }
+        else{
+            JRef<@Strong Product> currProd = FoundProducts.get(index);
+
+            ret = currProd.invoke("getName");
+            ret += currProd.invoke("getCost");
+
+            if(getComments){
+                String comments = currProd.invoke("getComments");
+                ret += comments;
+            }
+        }
+        if(printInfo)
+            System.out.println(ret);
+
+        return ret;
+    }
+
+    public boolean Checkout(boolean PrintReceipt){
+        Optional<JReplicaSystem> systemOptional = getSystem();
+        JReplicaSystem system;
+        if(systemOptional.isPresent())
+            system = systemOptional.get();
+        else
+            return false;
+
+        if(currentlyLoggedIn == null){
+            System.out.println("Please log in first");
+            return false;
+        }
+        if(currentlyLoggedIn.invoke("verifyLogin", system.toString())){
+            return CartOfLoggedIn.invoke("checkout", currentlyLoggedIn, PrintReceipt, system.toString());
+        }
+        System.out.println("Please log in first");
+        return false;
+    }
+
+    public double AddBalance(double value, boolean PrintBalance){
+        Optional<JReplicaSystem> systemOptional = getSystem();
+        JReplicaSystem system;
+        if(systemOptional.isPresent())
+            system = systemOptional.get();
+        else
+            return Double.NaN;
+
+        if(currentlyLoggedIn != null){
+            double newBalance = currentlyLoggedIn.invoke("addBalance", value, system.toString());
+            if(!Double.isNaN(newBalance)){
+                if(PrintBalance)
+                    System.out.println("Money added successfully, new balance: " + newBalance);
+                return newBalance;
+            }
+        }
+        return Double.NaN;
+    }
+}
---
+++ case-study-and-bench/case-study-strong/src/main/java/de.tuda.stg.consys.casestudystrong/User.java
@@ -0,0 +1,133 @@
+package de.tuda.stg.consys.casestudystrong;
+
+import de.tuda.stg.consys.checker.qual.Strong;
+import de.tuda.stg.consys.checker.qual.Weak;
+import de.tuda.stg.consys.objects.actors.AkkaReplicaSystem;
+import de.tuda.stg.consys.objects.japi.JConsistencyLevel;
+import de.tuda.stg.consys.objects.japi.JRef;
+import de.tuda.stg.consys.objects.japi.JReplicaSystem;
+import de.tuda.stg.consys.objects.japi.JReplicated;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class User implements Serializable , JReplicated {
+
+    /* This field is needed for JReplicated */
+    public transient AkkaReplicaSystem<String> replicaSystem = null;
+
+    private String userID;
+
+    private String password;
+
+    private LinkedList<String> loggedInFrom;
+
+    private String description;
+
+    private double balance;
+
+    private JRef<@Strong Cart> cart;
+
+    private LinkedList<JRef<@Strong Product>> buyHistory;
+
+    User(String userID, String password){
+        this.userID = userID; this.password = password;
+        loggedInFrom = new LinkedList<String>();
+        balance = 0;
+        buyHistory = new LinkedList<JRef<@Strong Product>>();
+    }
+
+    public boolean init(){
+        Optional<JReplicaSystem> systemOptional = getSystem();
+        JReplicaSystem system;
+        if(systemOptional.isPresent())
+            system = systemOptional.get();
+        else
+            return false;
+
+        this.cart = system.replicate(new Cart(system), JConsistencyLevel.STRONG);
+        return true;
+    }
+
+    public boolean Login(String userID, String password, String systemInfo){
+        boolean ret = false;
+        if(!loggedInFrom.contains(systemInfo)){
+            //Security is not the purpose of this case study, therefore I check passwords and userIDs like this.
+            //This is not the way to check passwords in any real Program.
+            if(userID.equals(this.userID) && password.equals(this.password)){
+                loggedInFrom.add(systemInfo);
+                ret = true;
+            }
+        }
+        return ret;
+    }
+
+    public boolean Logout(String systemInfo){
+        if(loggedInFrom.contains(systemInfo)){
+            loggedInFrom.remove(systemInfo);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean SetDescription(String description,String systemInfo){
+        if(loggedInFrom.contains(systemInfo)){
+            this.description = description;
+            return true;
+        }
+        return false;
+    }
+
+    public JRef<@Strong Cart> FetchCart(String systemInfo){
+        if(loggedInFrom.contains(systemInfo)){
+            return cart;
+        }
+        else return null;
+    }
+
+    public boolean verifyLogin(String systemInfo){
+        if(loggedInFrom.contains(systemInfo)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verifyCredentials(String Username, String Password) {
+        if (Username.equals(userID) && Password.equals(this.password))
+            return true;
+        return false;
+    }
+
+    public String getName(){
+        return userID;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public boolean buy(double cost, LinkedList<JRef<@Strong Product>> products, String systemInfo){
+        if(loggedInFrom.contains(systemInfo)){
+            if(cost <= balance){
+                buyHistory.addAll(products);
+                balance -= cost;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public double addBalance(double balance, String systemInfo){
+        if(loggedInFrom.contains(systemInfo)){
+            if(balance > 0){
+                this.balance += balance;
+                return this.balance;
+            }
+            return Double.NaN;
+        }
+        else
+            return Double.NaN;
+    }
+}
