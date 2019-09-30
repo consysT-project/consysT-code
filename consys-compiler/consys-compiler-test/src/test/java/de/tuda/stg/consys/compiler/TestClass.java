bd22dd72799d4e9ab8ed8064b80b07738f83ae49
Mirko Köhler <kohler.mirko@gmail.com>
2019-09-30 15:54:53 +0200

    added some compiler support for consys

---
+++ consys-compiler/consys-compiler-test/src/test/java/de/tuda/stg/consys/compiler/TestClass.java
@@ -0,0 +1,39 @@
+package de.tuda.stg.consys.compiler;
+
+import de.tuda.stg.consys.objects.japi.Demo;
+import de.tuda.stg.consys.objects.japi.JConsistencyLevel;
+import de.tuda.stg.consys.objects.japi.JRef;
+import de.tuda.stg.consys.objects.japi.JReplicaSystem;
+
+/**
+ * Created on 30.09.19.
+ *
+ * @author Mirko Köhler
+ */
+public class TestClass {
+
+	public static void test() throws Exception {
+
+		JReplicaSystem replicaSystem1 = JReplicaSystem.fromActorSystem(2552);
+		JReplicaSystem replicaSystem2 = JReplicaSystem.fromActorSystem(2553);
+
+		System.out.println("system1 = " + replicaSystem1);
+		System.out.println("system2 = " + replicaSystem2);
+
+		try {
+			replicaSystem1.addReplicaSystem("127.0.0.1", 2553);
+			replicaSystem2.addReplicaSystem("127.0.0.1", 2552);
+
+			JRef<String> ref1Strong = replicaSystem1.replicate("os", "Hello World!", JConsistencyLevel.STRONG);
+			JRef<String> ref2Strong = replicaSystem2.ref("os", String.class, JConsistencyLevel.STRONG);
+
+			char c = ref1Strong.ref().charAt(3);
+
+		} finally {
+			replicaSystem1.close();
+			replicaSystem2.close();
+		}
+
+	}
+
+}
