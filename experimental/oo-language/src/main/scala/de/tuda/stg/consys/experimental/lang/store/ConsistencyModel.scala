ef6cd1ae9c65934a8a63bf048bbbef515698bc8d
Mirko Köhler <kohler.mirko@gmail.com>
2019-12-11 14:37:36 +0100

    added strong cassandra consistency

--- experimental/oo-language/src/main/scala/de/tuda/stg/consys/experimental/lang/store/ConsistencyModel.scala
+++ experimental/oo-language/src/main/scala/de/tuda/stg/consys/experimental/lang/store/ConsistencyModel.scala
@@ -1,19 +1,18 @@
 package de.tuda.stg.consys.experimental.lang.store
 
+import scala.reflect.runtime.universe._
+
 /**
  * Created on 10.12.19.
  *
  * @author Mirko Köhler
  */
 trait ConsistencyModel {
-
 	type StoreType <: Store
-
 	val store : StoreType
 
 	import store._
 	def toLevel : ConsistencyLevel
-
-	def createLocal[T <: ObjType](addr : Addr, obj : T) : RefType[T]
-
+	def createRef[T <: ObjType : TypeTag](addr : Addr, obj : T) : RefType[T]
+	def lookupRef[T <: ObjType : TypeTag](addr : Addr) : RefType[T]
 }
--- experimental/oo-language/src/main/scala/de/tuda/stg/consys/experimental/lang/store/TxContext.scala
+++ experimental/oo-language/src/main/scala/de/tuda/stg/consys/experimental/lang/store/TxContext.scala
@@ -1,5 +1,6 @@
 package de.tuda.stg.consys.experimental.lang.store
 
+
 import scala.reflect.runtime.universe.TypeTag
 
 /**
@@ -9,12 +10,15 @@ import scala.reflect.runtime.universe.TypeTag
  */
 trait TxContext {
 	type StoreType <: Store
+	type ConsistencyLevel =  de.tuda.stg.consys.experimental.lang.store.ConsistencyLevel {type StoreType = TxContext.this.StoreType}
 
 	val store : StoreType
 	import store._
 
-	def replicate[T <: ObjType : TypeTag](addr : Addr, obj : T, level : ConsistencyLevel) : RefType[T] = ???
+	def replicate[T <: ObjType : TypeTag](addr : Addr, obj : T, level : ConsistencyLevel) : RefType[T] =
+		throw new UnsupportedOperationException("this context does not support replicating new objects")
 
-	def lookup[T <: ObjType : TypeTag](addr : Addr, level : ConsistencyLevel) : RefType[T] = ???
+	def lookup[T <: ObjType : TypeTag](addr : Addr, level : ConsistencyLevel) : RefType[T] =
+		throw new UnsupportedOperationException("this context does not support looking up existing objects")
 
 }
