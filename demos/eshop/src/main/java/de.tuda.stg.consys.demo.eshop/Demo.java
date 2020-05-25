2a09d2acf35dea0bd02298589b526a4d0c235e90
Mirko Köhler <kohler.mirko@googlemail.com>
2020-05-25 11:53:02 +0200

    demos work again

--- demos/eshop/src/main/java/de.tuda.stg.consys.demo.eshop/Demo.java
+++ demos/eshop/src/main/java/de.tuda.stg.consys.demo.eshop/Demo.java
@@ -1,10 +1,10 @@
-package de.tuda.stg.consys.demo.messagegroups;
+package de.tuda.stg.consys.demo.eshop;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import de.tuda.stg.consys.demo.DemoExecutor;
 
-public class Demo extends DemoExecutor<MessageGroupsBenchmark> {
+public class Demo extends DemoExecutor<EShopBenchmark> {
     public static void main(String[] args) throws Exception{
         new Demo().runDemo();
     }
@@ -14,21 +14,20 @@ public class Demo extends DemoExecutor<MessageGroupsBenchmark> {
         var configString = "consys {\n" +
                 "  bench {\n" +
                 "    demo {\n" +
-                "      messagegroups {\n" +
-                "        groups = 500\n" +
-                "        weakGroups = 0\n" +
+                "      eshop {\n" +
+                "        products = 100\n" +
+                "        users = 1000\n" +
                 "      }\n" +
                 "      type = \"mixed\"\n" +
                 "    }\n" +
                 "  }\n" +
-                "\n" +
                 "}";
 
         return ConfigFactory.parseString(configString);
     }
 
     @Override
-    protected Class<MessageGroupsBenchmark> benchmarkClass() {
-        return MessageGroupsBenchmark.class;
+    protected Class<EShopBenchmark> benchmarkClass() {
+        return EShopBenchmark.class;
     }
 }
