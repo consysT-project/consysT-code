7cfed35f913bb4a730864b6768465406182eb0f2
Julius Naeumann <julius.naeumann@gmail.com>
2020-06-06 11:25:34 +0200

    removed type variable from DeltaMergable

--- consys-core/src/main/scala/de/tuda/stg/consys/core/akka/DeltaCRDTAkkaReplicaSystem.scala
+++ consys-core/src/main/scala/de/tuda/stg/consys/core/akka/DeltaCRDTAkkaReplicaSystem.scala
@@ -1,9 +1,10 @@
 package de.tuda.stg.consys.core.akka
 import java.io
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem}
+
 import scala.reflect.runtime.universe._
-import de.tuda.stg.consys.core.akka.DeltaCRDTAkkaReplicaSystem.DeltaCRDTReplicatedObject
+import de.tuda.stg.consys.core.akka.DeltaCRDTAkkaReplicaSystem.{DeltaCRDTReplicatedObject, DeltaUpdateReq}
 import de.tuda.stg.consys.core.ConsistencyLabel.DCRDT
 import de.tuda.stg.consys.core.akka.Requests.{InvokeOp, NoAnswerRequest, Operation, Request, SynchronousRequest}
 
@@ -14,7 +15,10 @@ Author: Kris Frühwein und Julius Näumann
  */
 trait DeltaCRDTAkkaReplicaSystem extends AkkaReplicaSystem {
 
+
+
   override protected def createMasterReplica[T <: Obj : TypeTag](l: ConsistencyLevel, addr: Addr, obj: T): AkkaReplicatedObject[Addr, T] = l match {
+
     case DCRDT => new DeltaCRDTReplicatedObject[Addr, T](obj, addr, this)
     case _ => super.createMasterReplica[T](l, addr, obj)
   }
@@ -45,7 +49,7 @@ trait DeltaCRDTAkkaReplicaSystem extends AkkaReplicaSystem {
       case class DeltaUpdateReq(obj: Any) extends NoAnswerRequest
 
 
-      private[DeltaCRDTAkkaReplicaSystem] class DeltaCRDTReplicatedObject[Loc, T <: DeltaMergeable[T]]
+      private[DeltaCRDTAkkaReplicaSystem] class DeltaCRDTReplicatedObject[Loc, T <: DeltaCRDT]
       (
         init: T, val addr: Loc, val replicaSystem: AkkaReplicaSystem {type Addr = Loc}
       )(
@@ -54,12 +58,12 @@ trait DeltaCRDTAkkaReplicaSystem extends AkkaReplicaSystem {
         with Lockable[T]
         with Serializable {
         setObject(init)
-
         override final def consistencyLevel: ConsistencyLevel = DCRDT
 
         override def handleRequest[R](request: Request[R]): R = request match {
-          case DeltaUpdateReq(state: DeltaMergeable[T]#DeltaType@unchecked) =>
-            getObject.asInstanceOf[DeltaMergeable[T]].merge(state)
+          case DeltaUpdateReq(state: AkkaReplicaSystem#Obj) =>
+            getObject.merge(state)
+
             None.asInstanceOf[R]
           case _ =>
             super.handleRequest(request)
@@ -84,3 +88,14 @@ trait DeltaCRDTAkkaReplicaSystem extends AkkaReplicaSystem {
 
       }
     }
+
+abstract class DeltaCRDT extends DeltaMergeable {
+
+
+
+  def transmitDelta(delta: AkkaReplicaSystem#Obj) = {
+    AkkaReplicaSystems.system.foreachOtherReplica(handler => handler.request( DeltaUpdateReq(delta)))
+  }
+
+
+}
--- consys-core/src/main/scala/de/tuda/stg/consys/core/akka/DeltaMergeable.scala
+++ consys-core/src/main/scala/de/tuda/stg/consys/core/akka/DeltaMergeable.scala
@@ -1,12 +1,13 @@
 package de.tuda.stg.consys.core.akka
 
-trait DeltaMergeable[T] {
-  /*
-  DeltaType is the type of the delta message. If you have a counter and update with numbers, then Integer
-  would be the DeltaType.
-   */
-  type DeltaType
-  private[akka] def merge(other:DeltaType) //T ändern zu etwas anderem, dazu neuen Datentyp erstellen,
-                                   // "DeltaMessage" mit entsprechendem delta, bei erstellung angeben
+/**
+ * Classes with this trait allow being updated with a delta state
+ * @tparam T delta state type
+ */
+trait DeltaMergeable {
+
+  private[akka] def merge(other:AkkaReplicaSystem#Obj)
 
 }
+
+
