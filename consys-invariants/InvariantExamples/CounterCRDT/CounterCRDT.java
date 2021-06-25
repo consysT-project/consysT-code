6006957f6feb0c02709f92f1c326618eb1301771
Benyamin Delshad <benyamindelshad@gmail.com>
2021-06-25 16:03:59 +0200

    fixed CounterCRDT example

--- consys-invariants/InvariantExamples/CounterCRDT/CounterCRDT.java
+++ consys-invariants/InvariantExamples/CounterCRDT/CounterCRDT.java
@@ -1,22 +1,30 @@
 public class CounterCRDT {
-  public static final int numOfReplicas = 10;
-  public static final int replicaId = 5;
+  public static final int numOfReplicas = 3;
 
     /*@
-    @ public invariant getValue() >= 0;
+    @ public invariant (\sum int i; i >= 0 && i < numOfReplicas; incs[i]) - (\sum int i; i >= 0 && i < numOfReplicas; decs[i]) >= 0;
     @ public invariant (\forall int inv1; inv1>=0 && inv1<numOfReplicas;
                           incs[inv1] >=0);
     @ public invariant (\forall int inv2; inv2>=0 && inv2<numOfReplicas;
                           decs[inv2] >=0);
     @*/
-  int[] incs = new int[numOfReplicas];
-  int[] decs = new int[numOfReplicas];
+    public int[] incs;
+    public int[] decs;
+    public int replicaId;
 
-  /*@
-  @ ensures (\forall int init; init>=0 && init<numOfReplicas;
-              (incs[init] == 0) && (decs[init] == 0));
-  @*/
-  public CounterCRDT() {}
+  /* Constructors */
+  // Constructors define the initial state of an object.
+  //@ requires id >= 0 && id < numOfReplicas;
+  //@ ensures (\forall int i; i >= 0 && i < numOfReplicas; incs[i] == 0 && decs[i] == 0);
+  //@ ensures replicaId == id;
+  public CounterCRDT(int id) {
+    if (!(id >= 0 && id < numOfReplicas))
+      throw new IllegalArgumentException("id not in range.");
+
+    this.incs = new int[numOfReplicas];
+    this.decs = new int[numOfReplicas];
+    this.replicaId = id;
+  }
 
 
   /*@
@@ -24,49 +32,65 @@ public class CounterCRDT {
   @ ensures \result ==
             (\sum int incInd; incInd>=0 && incInd<numOfReplicas; incs[incInd]);
   @*/
-  int sumIncs() { return 0; }
+  int sumIncs() {
+    int res = 0;
+    for (int inc : incs) {
+      res += inc;
+    }
+    return res;
+  }
 
   /*@
   @ assignable \nothing;
   @ ensures \result ==
             (\sum int decInd; decInd>=0 && decInd<numOfReplicas; decs[decInd]);
   @*/
-  int sumDecs(){ return 0; }
+  int sumDecs(){
+    int result = 0;
+    for (int dec : decs) {
+      result += dec;
+    }
+    return result;
+  }
 
-  /*@ ensures \result == sumIncs() - sumDecs();
-  @ assignable \nothing;
+  /*@ assignable \nothing;
+  @ ensures \result == (\sum int i; i >= 0 && i < numOfReplicas; incs[i]) - (\sum int i; i >= 0 && i < numOfReplicas; decs[i]);
   @*/
-  int getValue() { return 0; }
+  int getValue() { return sumIncs() - sumDecs(); }
 
 
 
   /*@
+  @ assignable incs[replicaId];
   @ ensures incs[replicaId] == \old(incs[replicaId]) + 1;
-  @ ensures (\forall int incI; incI>=0 && incI<numOfReplicas && incI != replicaId;
-              incs[incI] == \old(incs[incI]));
-  @ ensures decs == \old(decs);
   @*/
-  void inc() {}
+  void inc() {
+    incs[replicaId] = incs[replicaId] + 1;
+  }
 
   /*@
-  @ requires \old(getValue()) >= 1;
+  @ requires (\sum int i; i >= 0 && i < numOfReplicas; incs[i]) - (\sum int i; i >= 0 && i < numOfReplicas; decs[i]) >= 1;
+  @ assignable decs[replicaId];
   @ ensures decs[replicaId] == \old(decs[replicaId]) + 1;
-  @ ensures (\forall int decI; decI>=0 && decI<numOfReplicas && decI != replicaId;
-              decs[decI] == \old(decs[decI]));
-  @ ensures incs == \old(incs);
   @*/
-  void dec() {}
+  void dec() {
+    if (val > getValue())
+      throw new IllegalArgumentException("not enough balance to withdraw");
+    decs[replicaId] = decs[replicaId] + 1;
+  }
 
   /*@
-  @ ensures \old(sumIncs()) > other.sumIncs() ==> incs == \old(incs);
-  @ ensures \old(sumIncs()) < other.sumIncs() ==> incs == other.incs;
-  @ ensures \old(sumIncs()) == other.sumIncs()
-              ==> incs == \old(incs) && incs == other.incs;
-  @
-  @ ensures \old(sumDecs()) > other.sumDecs() ==> decs == \old(decs);
-  @ ensures \old(sumDecs()) < other.sumDecs() ==> decs == other.decs;
-  @ ensures \old(sumDecs()) == other.sumDecs()
-              ==> decs == \old(decs) && decs == other.decs;
+    @ requires ((\sum int i; i >= 0 && i < numOfReplicas; incs[i] >= other.incs[i] ? incs[i] : other.incs[i] )
+             - (\sum int i; i >= 0 && i < numOfReplicas; decs[i] >= other.decs[i] ? decs[i] : other.decs[i] )) >= 0;
+    @ ensures (\forall int i; i >= 0 && i < numOfReplicas;
+                   (\old(incs[i]) >= other.incs[i] ? incs[i] == \old(incs[i]) : incs[i] == other.incs[i])
+                && (\old(decs[i]) >= other.decs[i] ? decs[i] == \old(decs[i]) : decs[i] == other.decs[i]));
+    @ ensures replicaId == \old(replicaId);
   @*/
-  void merge(CounterCRDT other) {}
+  void merge(CounterCRDT other) {
+    for (int i = 0; i < numOfReplicas; i++) {
+      incs[i] = Math.max(incs[i], other.incs[i]);
+      decs[i] = Math.max(decs[i], other.decs[i]);
+    }
+  }
 }
