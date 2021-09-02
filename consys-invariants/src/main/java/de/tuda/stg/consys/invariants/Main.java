7134f824d3802cc940df1d5259857674a01427d3
Benyamin Delshad <benyamindelshad@gmail.com>
2021-09-02 18:22:41 +0200

    updated benchmarks after some changes happened at use cases

--- consys-invariants/src/main/java/de/tuda/stg/consys/invariants/Main.java
+++ consys-invariants/src/main/java/de/tuda/stg/consys/invariants/Main.java
@@ -82,6 +82,7 @@ public class Main {
     };
     // other use cases: ------------------------------
     Path[] benchSource2 = new Path[] {
+            Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/gset/GSet.java"),
             Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/bankaccount/BankAccount.java"),
             Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/multicounter/SimpleNumber.java"),
             Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/multicounter/SimpleCounter.java"),
@@ -94,10 +95,17 @@ public class Main {
             Paths.get("consys-riak/src/main/java/com/readytalk/crdt/sets/GSet.java"),
             Paths.get("consys-riak/src/main/java/com/readytalk/crdt/sets/TwoPhaseSet.java"),
             Paths.get("consys-riak/src/main/java/com/readytalk/crdt/sets/ORSet.java"),
+            Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/tournament/Player.java"),
+            Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/tournament/GSetPlayer.java"),
+            Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/tournament/TwoPhaseSetPlayer.java"),
+            Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/tournament/Tournament.java"),
+            Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/tournament/GSetTournament.java"),
+            Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/tournament/TwoPhaseSetTournament.java"),
+            Paths.get("consys-invariants/src/main/examples/de/tuda/stg/consys/invariants/examples/tournament/Tournaments.java")
     };
 
-    int numOfRounds = 0; // change it for benchmarks
-    int extra = 9; // extra use cases other than benchSource singleClass use cases.
+    int numOfRounds = 10; // change it for benchmarks
+    int extra = 11; // extra use cases other than benchSource singleClass use cases.
     double totalTime[] = new double[benchSource.length + extra];
     String classNames[] = new String[benchSource.length + extra];
     int index = 0;
@@ -115,67 +123,78 @@ public class Main {
       totalTime[index] = benchmark(config, inputSource, numOfRounds);
       classNames[index] = benchSource[index].toString().substring(benchSource[index].toString().lastIndexOf('/') + 1, benchSource[index].toString().length() - 5);
     }
-
-    // Bank Account: ----------------------------------
+    // GSet: ----------------------------------
     inputSource = new Path[1];
     inputSource[0] = benchSource2[0];
+    totalTime[index] = benchmark(config, inputSource, numOfRounds);
+    classNames[index] = "GSet";
+    index += 1;
+    // Bank Account: ----------------------------------
+    inputSource = new Path[1];
+    inputSource[0] = benchSource2[1];
     totalTime[index] = benchmark(config, inputSource, numOfRounds);
     classNames[index] = "BankAccount";
     index += 1;
     // MultiClassCounter: ------------------------------------------
     inputSource = new Path[2];
-    inputSource[0] = benchSource2[1];
-    inputSource[1] = benchSource2[2];
+    inputSource[0] = benchSource2[2];
+    inputSource[1] = benchSource2[3];
     totalTime[index] = benchmark(config, inputSource, numOfRounds);
     classNames[index] = "MultiClassCounter";
     index += 1;
     // SequentialCreditAccount: ------------------------------------
     inputSource = new Path[2];
-    inputSource[0] = benchSource2[3];
-    inputSource[1] = benchSource2[4];
+    inputSource[0] = benchSource2[4];
+    inputSource[1] = benchSource2[5];
     totalTime[index] = benchmark(config, inputSource, numOfRounds);
     classNames[index] = "SequentialCreditAccount";
     index += 1;
     // ReplicatedCreditAccount: ------------------------------------
     inputSource = new Path[2];
-    inputSource[0] = benchSource2[5];
-    inputSource[1] = benchSource2[6];
+    inputSource[0] = benchSource2[6];
+    inputSource[1] = benchSource2[7];
     totalTime[index] = benchmark(config, inputSource, numOfRounds);
     classNames[index] = "ReplicatedCreditAccount";
     index += 1;
     // Riak-GCounter: ------------------------------------
     inputSource = new Path[1];
-    inputSource[0] = benchSource2[7];
+    inputSource[0] = benchSource2[8];
     totalTime[index] = benchmark(config, inputSource, numOfRounds);
     classNames[index] = "Riak:GCounter";
     index += 1;
-    // ReplicatedCreditAccount: ------------------------------------
+    // Riak-PNCounter: ------------------------------------
     inputSource = new Path[2];
-    inputSource[0] = benchSource2[7];
-    inputSource[1] = benchSource2[8];
+    inputSource[0] = benchSource2[8];
+    inputSource[1] = benchSource2[9];
     totalTime[index] = benchmark(config, inputSource, numOfRounds);
     classNames[index] = "Riak:PNCounter";
     index += 1;
     // Riak-GSet: ------------------------------------
     inputSource = new Path[1];
-    inputSource[0] = benchSource2[9];
+    inputSource[0] = benchSource2[10];
     totalTime[index] = benchmark(config, inputSource, numOfRounds);
     classNames[index] = "Riak:GSet";
     index += 1;
-    // ReplicatedCreditAccount: ------------------------------------
+    // Riak-TwoPhaseSet: ------------------------------------
     inputSource = new Path[2];
-    inputSource[0] = benchSource2[9];
-    inputSource[1] = benchSource2[10];
+    inputSource[0] = benchSource2[10];
+    inputSource[1] = benchSource2[11];
     totalTime[index] = benchmark(config, inputSource, numOfRounds);
     classNames[index] = "Riak:TwoPhaseSet";
     index += 1;
-    // Riak-GSet: ------------------------------------
+    // Riak-ORSet: ------------------------------------
     inputSource = new Path[1];
-    inputSource[0] = benchSource2[11];
+    inputSource[0] = benchSource2[12];
     totalTime[index] = benchmark(config, inputSource, numOfRounds);
     classNames[index] = "Riak:ORSet";
     index += 1;
-    // Printing: ------
+    // Tournaments: ------------------------------------
+    inputSource = new Path[7];
+    for(int i = 0; i < 7; i += 1) inputSource[i] = benchSource2[13 + i];
+    totalTime[index] = benchmark(config, inputSource, numOfRounds);
+    classNames[index] = "Tournaments";
+    index += 1;
+    // Printing: ---------------------------------------
     if(numOfRounds > 0) {
       System.out.println();
       System.out.println("-----------------------Benchmarks-----------------------");
@@ -188,41 +207,49 @@ public class Main {
     // Maybe Tournaments?
     /*
     Last result:
-    Number of use cases: 15
-    Number of rounds: 10
-    Average verifying time for the use case BankAccountLWW: 112.7 ms.
-    Average verifying time for the use case Consensus: 69.9 ms.
-    Average verifying time for the use case DistributedLock: 42.2 ms.
-    Average verifying time for the use case GCounterCRDT: 206.4 ms.
-    Average verifying time for the use case PNCounterCRDT: 165.8 ms.
-    Average verifying time for the use case ResettableCounterWithRound: 88.1 ms.
-    Average verifying time for the use case BankAccount: 444.8 ms.
-    Average verifying time for the use case MultiClassCounter: 86.8 ms.
-    Average verifying time for the use case SequentialCreditAccount: 31.8 ms.
-    Average verifying time for the use case ReplicatedCreditAccount: 500.3 ms.
-    Average verifying time for the use case Riak:GCounter: 4361.4 ms.
-    Average verifying time for the use case Riak:PNCounter: 301851.9 ms.
-    Average verifying time for the use case Riak:GSet: 34.2 ms.
-    Average verifying time for the use case Riak:TwoPhaseSet: 58.7 ms.
-    Average verifying time for the use case Riak:ORSet: 54.7 ms.
+-----------------------Benchmarks-----------------------
+Number of use cases: 17
+Number of rounds: 10
+Average verifying time for the use case BankAccountLWW: 30.0 ms.
+Average verifying time for the use case Consensus: 34.5 ms.
+Average verifying time for the use case DistributedLock: 39.7 ms.
+Average verifying time for the use case GCounterCRDT: 162.1 ms.
+Average verifying time for the use case PNCounterCRDT: 162.9 ms.
+Average verifying time for the use case ResettableCounterWithRound: 86.7 ms.
+Average verifying time for the use case GSet: 27.1 ms.
+Average verifying time for the use case BankAccount: 440.6 ms.
+Average verifying time for the use case MultiClassCounter: 94.6 ms.
+Average verifying time for the use case SequentialCreditAccount: 28.9 ms.
+Average verifying time for the use case ReplicatedCreditAccount: 504.3 ms.
+Average verifying time for the use case Riak:GCounter: 113.2 ms.
+Average verifying time for the use case Riak:PNCounter: 111.6 ms.
+Average verifying time for the use case Riak:GSet: 36.5 ms.
+Average verifying time for the use case Riak:TwoPhaseSet: 63.3 ms.
+Average verifying time for the use case Riak:ORSet: 54.3 ms.
+Average verifying time for the use case Tournaments: 48.2 ms.
 
     Last result for compiling:
-    Number of rounds: 10
-    Average verifying time for the use case BankAccountLWW: 28.0 ms.
-    Average verifying time for the use case Consensus: 29.0 ms.
-    Average verifying time for the use case DistributedLock: 24.8 ms.
-    Average verifying time for the use case GCounterCRDT: 19.1 ms.
-    Average verifying time for the use case PNCounterCRDT: 16.2 ms.
-    Average verifying time for the use case ResettableCounterWithRound: 16.5 ms.
-    Average verifying time for the use case BankAccount: 22.8 ms.
-    Average verifying time for the use case MultiClassCounter: 19.2 ms.
-    Average verifying time for the use case SequentialCreditAccount: 18.3 ms.
-    Average verifying time for the use case ReplicatedCreditAccount: 18.0 ms.
-    Average verifying time for the use case Riak:GCounter: 27.2 ms.
-    Average verifying time for the use case Riak:PNCounter: 24.7 ms.
-    Average verifying time for the use case Riak:GSet: 25.3 ms.
-    Average verifying time for the use case Riak:TwoPhaseSet: 38.8 ms.
-    Average verifying time for the use case Riak:ORSet: 38.9 ms.
+
+-----------------------Benchmarks-----------------------
+Number of use cases: 17
+Number of rounds: 10
+Average verifying time for the use case BankAccountLWW: 22.8 ms.
+Average verifying time for the use case Consensus: 28.3 ms.
+Average verifying time for the use case DistributedLock: 15.3 ms.
+Average verifying time for the use case GCounterCRDT: 18.3 ms.
+Average verifying time for the use case PNCounterCRDT: 19.5 ms.
+Average verifying time for the use case ResettableCounterWithRound: 12.4 ms.
+Average verifying time for the use case GSet: 18.5 ms.
+Average verifying time for the use case BankAccount: 26.1 ms.
+Average verifying time for the use case MultiClassCounter: 22.0 ms.
+Average verifying time for the use case SequentialCreditAccount: 15.7 ms.
+Average verifying time for the use case ReplicatedCreditAccount: 17.8 ms.
+Average verifying time for the use case Riak:GCounter: 26.4 ms.
+Average verifying time for the use case Riak:PNCounter: 31.2 ms.
+Average verifying time for the use case Riak:GSet: 24.5 ms.
+Average verifying time for the use case Riak:TwoPhaseSet: 33.2 ms.
+Average verifying time for the use case Riak:ORSet: 32.9 ms.
+Average verifying time for the use case Tournaments: 36.2 ms.
 
     */
 
@@ -243,11 +270,11 @@ public class Main {
     var compileResult = CompilerBinding.compile(additionalClasspath, sources);
 
     // Create the program modelconsys
-    var model = new ProgramModel(compileResult, config);
-    model.loadParsedClasses();
+    //var model = new ProgramModel(compileResult, config);
+    //model.loadParsedClasses();
 
     // Check the classes
-    model.checkAll();
+    //model.checkAll();
   }
 
 
