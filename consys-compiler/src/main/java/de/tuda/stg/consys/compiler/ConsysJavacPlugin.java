61d2f51b904b4a1ced23f964a289e6a89551b204
Niklas Reiche <29310846+NiklasReiche@users.noreply.github.com>
2022-04-29 17:54:43 +0200

    Move compiler plugin to analyze phase

--- consys-compiler/src/main/java/de/tuda/stg/consys/compiler/ConsysJavacPlugin.java
+++ consys-compiler/src/main/java/de/tuda/stg/consys/compiler/ConsysJavacPlugin.java
@@ -7,6 +7,7 @@ import com.sun.source.util.Plugin;
 import com.sun.source.util.TaskEvent;
 import com.sun.source.util.TaskListener;
 import com.sun.tools.javac.api.BasicJavacTask;
+import com.sun.tools.javac.code.*;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
 import com.sun.tools.javac.util.Context;
@@ -14,7 +15,9 @@ import com.sun.tools.javac.util.Log;
 import com.sun.tools.javac.util.Names;
 
 import javax.lang.model.element.Name;
+import javax.lang.model.type.TypeKind;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 
@@ -45,7 +48,7 @@ public class ConsysJavacPlugin implements Plugin {
 			@Override
 			public void finished(TaskEvent taskEvent) {
 				//We need to listen to the ANALYZE phase because we need type information
-				if (taskEvent.getKind() != TaskEvent.Kind.PARSE) return;
+				if (taskEvent.getKind() != TaskEvent.Kind.ANALYZE) return;
 
 				new ModifyingTreePathScanner(context) {
 
@@ -77,20 +80,47 @@ public class ConsysJavacPlugin implements Plugin {
 									args[i] = (JCTree.JCExpression) methodInv.arguments.get(i - 1);
 								}
 
-								JCTree.JCFieldAccess newSelect = factory.Select((JCTree.JCExpression) methodInv.expr, names.fromString("invoke"));
-//								Types.instance(context).createMethodTypeWithReturn()
-//								newSelect.setType(((JCTree)tree).type);
-//								newSelect.sym = //((JCTree.JCFieldAccess)tree).sym;
-//									new Symbol.MethodSymbol(0L, names.fromString("invoke"), ((JCTree) methodInv.originalPath.leaf).type,
-//										new Symbol.ClassSymbol(0L, names.fromString("de.tuda.stg.consys.objects.japi.JRef"), ((JCTree.JCFieldAccess) tree).selected.type, null));
+								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "old tree: " + tree);
+								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "old type: " + ((JCTree)tree).type);
+								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "old symbol: " + ((JCTree.JCFieldAccess)tree).sym);
+								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "old symbol type: " + ((JCTree.JCFieldAccess)tree).sym.type);
+								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "old symbol flags: " + Flags.asFlagSet(((JCTree.JCFieldAccess)tree).sym.flags()));
 
+
+								JCTree.JCFieldAccess newSelect = factory.Select((JCTree.JCExpression) methodInv.expr, names.fromString("invoke"));
+
+								//Type newType = Types.instance(context).createMethodTypeWithParameters(
+								//		methodInv.type, methodInv.type.getParameterTypes().prepend(Symtab.instance(context).stringType));
+								Type newType = Types.instance(context).createMethodTypeWithReturn(methodInv.type, Symtab.instance(context).objectType);
+								Type.ArrayType arrayType = new Type.ArrayType(Symtab.instance(context).objectType, Symtab.instance(context).arrayClass);
+								newType = Types.instance(context).createMethodTypeWithParameters(
+										newType, com.sun.tools.javac.util.List.of(Symtab.instance(context).stringType, arrayType));
+
+								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "new arg types: " + newType.getParameterTypes().toString(","));
+								newSelect.setType(newType);
+
+								newSelect.sym = //((JCTree.JCFieldAccess)tree).sym;
+									new Symbol.MethodSymbol(Flags.PUBLIC | Flags.ABSTRACT, names.fromString("invoke"), newType, ((JCTree.JCFieldAccess)tree).sym.owner);
+										//new Symbol.ClassSymbol(0L, names.fromString("de.tuda.stg.consys.objects.japi.Ref"), ((JCTree.JCFieldAccess) tree).selected.type,
+										//		((JCTree.JCFieldAccess)tree).sym.owner.owner));
+
+								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "new type: " + newType);
+								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "new symbol: " + newSelect.sym);
+
+								var argList = com.sun.tools.javac.util.List.from(args);
+								JCTree.JCExpression arrayArg = factory.NewArray(factory.Type(Symtab.instance(context).objectType), com.sun.tools.javac.util.List.nil(), argList.tail);
+								arrayArg.setType(arrayType);
+								argList = com.sun.tools.javac.util.List.from(new JCTree.JCExpression[]{args[0], arrayArg});
 
 								JCTree.JCMethodInvocation newInvoke = factory.at( ((JCTree) methodInv.originalPath.getLeaf()).pos )
 								.Apply(null,
 									newSelect,
-									com.sun.tools.javac.util.List.from(args)
+									argList //com.sun.tools.javac.util.List.from(args)
 								);
-//								newInvoke.type = ((JCTree) methodInv.originalPath.leaf).type;
+								newInvoke.type = ((JCTree) methodInv.originalPath.getLeaf()).type; // TODO: correct return type or must be Object?
+								//newInvoke.type = newType.getReturnType();
+								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "New invoke: " + newInvoke);
+								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "New invoke type: " + newInvoke.type);
 
 								if (DEBUG) Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "Old invoke: " + methodInv.originalPath.getLeaf());
 //								Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "Path: " + methodInv.originalPath);
@@ -115,7 +145,7 @@ public class ConsysJavacPlugin implements Plugin {
 									);
 
 								if (DEBUG) Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "Old assign: " + assign.originalPath.getLeaf());
-								assign.originalPath.getModificator().accept(newSetField);
+								//assign.originalPath.getModificator().accept(newSetField);
 								if (DEBUG) Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "New assign: " + newSetField);
 
 							} else if (refUsage instanceof CFieldAcc) {
@@ -135,7 +165,7 @@ public class ConsysJavacPlugin implements Plugin {
 									);
 
 								if (DEBUG) Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "Old access: " + fieldAcc.originalPath.getLeaf());
-								fieldAcc.originalPath.getModificator().accept(newGetField);
+								//fieldAcc.originalPath.getModificator().accept(newGetField);
 								if (DEBUG) Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "New access: " + newGetField);
 
 							}
@@ -174,7 +204,7 @@ public class ConsysJavacPlugin implements Plugin {
 		//TODO: Types are not available in PARSE phase, but in ANALYZE phase we have to provide types when creating new expressions
 //		Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "Type of expr: " + ((JCTree.JCExpression) expr).type);
 
-		//Move to parent leaf
+		//Move to parent leaf -> Ref.ref()
 		path = path.getParentPath();
 		if (path == null) {
 			if (DEBUG) Log.instance(context).printRawLines(Log.WriterKind.NOTICE,"=> empty2");
@@ -189,7 +219,7 @@ public class ConsysJavacPlugin implements Plugin {
 			return Optional.empty();
 		}
 
-		//Move to parent leaf
+		//Move to parent leaf -> Ref.ref().method
 		path = path.getParentPath();
 		if (path == null) {
 			if (DEBUG) Log.instance(context).printRawLines(Log.WriterKind.NOTICE,"=> empty4");
@@ -201,6 +231,7 @@ public class ConsysJavacPlugin implements Plugin {
 		if (curr instanceof MemberSelectTree) {
 			Name name = ((MemberSelectTree) curr).getIdentifier();
 
+			// move to parent -> Ref.ref().method()
 			ModifyingTreePath methodPath = path.getParentPath();
 			if (methodPath == null) return Optional.empty();
 			Tree methodCurr = methodPath.getLeaf();
@@ -208,7 +239,14 @@ public class ConsysJavacPlugin implements Plugin {
 			if (methodCurr instanceof MethodInvocationTree && ((MethodInvocationTree) methodCurr).getMethodSelect() == curr) {
 				List<? extends ExpressionTree> args = ((MethodInvocationTree) methodCurr).getArguments();
 				if (DEBUG) Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "=> method: " + expr + "." + name + "(" + args + ")");
-				return Optional.of(new CMethodInv(methodPath, expr, name, args));
+				//Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "-- expr: " + methodCurr);
+				//Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "-- Type of expr: " + ((JCTree.JCExpression) methodCurr).type);
+
+				//Log.instance(context).printRawLines(Log.WriterKind.NOTICE, "-- Type of expr: " + ((JCTree.JCExpression)expr).type);
+				//Type.ClassType t = (Type.ClassType) ((JCTree.JCExpression)expr).type;
+
+
+				return Optional.of(new CMethodInv(methodPath, expr, name, args, ((JCTree.JCFieldAccess)curr).type));
 			}
 		}
 
@@ -247,12 +285,14 @@ public class ConsysJavacPlugin implements Plugin {
 		final ExpressionTree expr;
 		final Name methodName;
 		final List<? extends ExpressionTree> arguments;
+		final Type type;
 
-		private CMethodInv(ModifyingTreePath originalPath, ExpressionTree expr, Name methodName, List<? extends ExpressionTree> arguments) {
+		private CMethodInv(ModifyingTreePath originalPath, ExpressionTree expr, Name methodName, List<? extends ExpressionTree> arguments, Type type) {
 			this.originalPath = originalPath;
 			this.expr = expr;
 			this.methodName = methodName;
 			this.arguments = arguments;
+			this.type = type;
 		}
 
 		@Override
---
+++ demos/box/src/main/java/Test.java
@@ -0,0 +1,63 @@
+import de.tuda.stg.consys.annotations.Transactional;
+import de.tuda.stg.consys.checker.qual.Immutable;
+import de.tuda.stg.consys.checker.qual.Inconsistent;
+import de.tuda.stg.consys.checker.qual.Mutable;
+import de.tuda.stg.consys.checker.qual.Strong;
+import de.tuda.stg.consys.japi.Ref;
+import de.tuda.stg.consys.japi.binding.cassandra.Cassandra;
+import de.tuda.stg.consys.japi.binding.cassandra.CassandraConsistencyLevels;
+import de.tuda.stg.consys.japi.binding.cassandra.CassandraStoreBinding;
+import de.tuda.stg.consys.japi.binding.cassandra.CassandraTransactionContextBinding;
+import org.checkerframework.dataflow.qual.SideEffectFree;
+import scala.Option;
+import scala.concurrent.duration.Duration;
+
+import java.io.PrintStream;
+import java.io.Serializable;
+
+public @Strong class Test implements Serializable {
+    Test f;
+
+    public static void main(String[] args) {
+        CassandraStoreBinding r0 = (@Inconsistent @Mutable CassandraStoreBinding)Cassandra.newReplica("127.0.0.1", 9042, 2181, Duration.apply(60000L, "ms"), true);
+        r0.transaction(ctx -> {
+            Ref<Test> r = ctx.replicate("t", CassandraConsistencyLevels.STRONG, Test.class);
+            @Strong int i = r.ref().getI() + r.ref().getI();
+            ((@Mutable @Inconsistent PrintStream)System.out).println(i);
+
+            return Option.empty();
+        });
+
+        try {
+            r0.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Transactional
+    void test(CassandraTransactionContextBinding tr, Ref<Test> tRef) {
+        //this.t("");
+        //tRef.ref().test1(tRef.ref().get());
+        //@Strong int i = tRef.ref().getI() + tRef.ref().getI();
+        //System.out.println(i);
+    }
+
+    void test1(Test t) {
+
+    }
+
+    void t(String s) {}
+
+    @Transactional
+    @SideEffectFree
+    Test get() {
+        return new Test();
+    }
+
+    @Transactional
+    @SideEffectFree
+    public @Strong int getI() {
+        return 1;
+    }
+}
