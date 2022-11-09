c58389c8a97aca664796a86e599e129bae074a80
Mirko Köhler <mail@mirko-koehler.de>
2022-11-09 09:23:12 +0100

    aws

--- execute-aws-bench.sh
+++ execute-aws-bench.sh
@@ -15,10 +15,10 @@ CONF=$1
 #java -cp demos/message-groups/target/message-groups-4.0.0-allinone.jar de.tuda.stg.consys.demo.messagegroups.MessageGroupsBenchmark -b cassandra -c aws/weak/${CONF}
 #echo "messagegroups strong"
 #java -cp demos/message-groups/target/message-groups-4.0.0-allinone.jar de.tuda.stg.consys.demo.messagegroups.MessageGroupsBenchmark -b cassandra -c aws/strong/${CONF}
-echo "messagegroups op_mixed"
-java -cp demos/message-groups/target/message-groups-4.0.0-allinone.jar de.tuda.stg.consys.demo.messagegroups.MessageGroupsBenchmark -b cassandra -c aws/op_mixed/${CONF}
-echo "messagegroups mixed"
-java -cp demos/message-groups/target/message-groups-4.0.0-allinone.jar de.tuda.stg.consys.demo.messagegroups.MessageGroupsBenchmark -b cassandra -c aws/mixed/${CONF}
+#echo "messagegroups op_mixed"
+#java -cp demos/message-groups/target/message-groups-4.0.0-allinone.jar de.tuda.stg.consys.demo.messagegroups.MessageGroupsBenchmark -b cassandra -c aws/op_mixed/${CONF}
+#echo "messagegroups mixed"
+#java -cp demos/message-groups/target/message-groups-4.0.0-allinone.jar de.tuda.stg.consys.demo.messagegroups.MessageGroupsBenchmark -b cassandra -c aws/mixed/${CONF}
 
 #echo "twitter weak"
 #java -cp demos/twitter-clone/target/twitter-clone-4.0.0-allinone.jar de.tuda.stg.consys.demo.twitterclone.TwitterCloneBenchmark -b cassandra -c aws/weak/${CONF}
@@ -50,11 +50,11 @@ java -cp demos/message-groups/target/message-groups-4.0.0-allinone.jar de.tuda.s
 #java -cp demos/rubis/target/rubis-4.0.0-allinone.jar de.tuda.stg.consys.demo.rubis.RubisBenchmark -b cassandra -c aws/weak/${CONF}
 #echo "rubis strong"
 #java -cp demos/rubis/target/rubis-4.0.0-allinone.jar de.tuda.stg.consys.demo.rubis.RubisBenchmark -b cassandra -c aws/strong/${CONF}
-echo "rubis op_mixed"
-java -cp demos/rubis/target/rubis-4.0.0-allinone.jar de.tuda.stg.consys.demo.rubis.RubisBenchmark -b cassandra -c aws/op_mixed/${CONF}
-echo "rubis mixed"
-java -cp demos/rubis/target/rubis-4.0.0-allinone.jar de.tuda.stg.consys.demo.rubis.RubisBenchmark -b cassandra -c aws/mixed/${CONF}
-echo "rubis weak_data"
-java -cp demos/rubis/target/rubis-4.0.0-allinone.jar de.tuda.stg.consys.demo.rubis.RubisBenchmark -b cassandra -c aws/weak_datacentric/${CONF}
+#echo "rubis op_mixed"
+#java -cp demos/rubis/target/rubis-4.0.0-allinone.jar de.tuda.stg.consys.demo.rubis.RubisBenchmark -b cassandra -c aws/op_mixed/${CONF}
+#echo "rubis mixed"
+#java -cp demos/rubis/target/rubis-4.0.0-allinone.jar de.tuda.stg.consys.demo.rubis.RubisBenchmark -b cassandra -c aws/mixed/${CONF}
+#echo "rubis weak_data"
+#java -cp demos/rubis/target/rubis-4.0.0-allinone.jar de.tuda.stg.consys.demo.rubis.RubisBenchmark -b cassandra -c aws/weak_datacentric/${CONF}
 echo "rubis strong_data"
 java -cp demos/rubis/target/rubis-4.0.0-allinone.jar de.tuda.stg.consys.demo.rubis.RubisBenchmark -b cassandra -c aws/strong_datacentric/${CONF}
