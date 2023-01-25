3afe715999f84a3d78e3f020afd83798282d528a
Mirko Köhler <mail@mirko-koehler.de>
2023-01-25 16:16:44 +0100

    some type changes in group chat

--- demos/group-chat/src/main/java/de/tuda/stg/consys/demo/messagegroups/schema/Chat.java
+++ demos/group-chat/src/main/java/de/tuda/stg/consys/demo/messagegroups/schema/Chat.java
@@ -2,10 +2,7 @@ package de.tuda.stg.consys.demo.messagegroups.schema;
 
 import de.tuda.stg.consys.annotations.Transactional;
 import de.tuda.stg.consys.annotations.methods.*;
-import de.tuda.stg.consys.checker.qual.Immutable;
-import de.tuda.stg.consys.checker.qual.Mutable;
-import de.tuda.stg.consys.checker.qual.Strong;
-import de.tuda.stg.consys.checker.qual.Weak;
+import de.tuda.stg.consys.checker.qual.*;
 import de.tuda.stg.consys.japi.Ref;
 import org.checkerframework.dataflow.qual.SideEffectFree;
 
@@ -20,10 +17,11 @@ import java.util.List;
  */
 public class Chat implements Serializable {
     private final List<Ref<@Mutable @Weak User>> users = new LinkedList<>();
-    private Counter numOfUsers = new Counter();
-    private Ref<@Strong ChatConfig> config;
+    private Ref<@Mutable @Strong Counter> numOfUsers;
+    private Ref<@Mutable @Weak ChatConfig> config;
 
-    public Chat(Ref<@Strong ChatConfig> config) {
+    public Chat(Ref<@Mutable @Strong Counter> numOfUsers,  Ref<@Mutable @Weak ChatConfig> config) {
+        this.numOfUsers = numOfUsers;
         this.config = config;
     }
 
@@ -36,8 +34,8 @@ public class Chat implements Serializable {
 
     @Transactional
     public void addUser(Ref<@Mutable @Weak User> user) {
-        if (numOfUsers.getValue() < config.ref().capacity) {
-            numOfUsers.inc();
+        if (numOfUsers.ref().getValue() < config.ref().capacity) {
+            numOfUsers.ref().inc();
             users.add(user);
         }
     }
--- demos/group-chat/src/main/java/de/tuda/stg/consys/demo/messagegroups/schema/ChatConfig.java
+++ demos/group-chat/src/main/java/de/tuda/stg/consys/demo/messagegroups/schema/ChatConfig.java
@@ -1,9 +1,11 @@
 package de.tuda.stg.consys.demo.messagegroups.schema;
 
-public class ChatConfig {
-    public final int capacity;
+import de.tuda.stg.consys.checker.qual.ThisConsistent;
 
-    public ChatConfig(int capacity) {
+public class ChatConfig {
+    public final @ThisConsistent int capacity;
+
+    public ChatConfig(@ThisConsistent  int capacity) {
         this.capacity = capacity;
     }
 }
