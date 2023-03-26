5ff0e59147b86290fa052ed530af221996e5b70c
Niklas Reiche <29310846+NiklasReiche@users.noreply.github.com>
2023-03-26 16:22:54 +0200

    Fix type-checker caching

--- consys-type-checker/src/main/scala/de/tuda/stg/consys/checker/ConsistencyAnnotatedTypeFactory.scala
+++ consys-type-checker/src/main/scala/de/tuda/stg/consys/checker/ConsistencyAnnotatedTypeFactory.scala
@@ -20,16 +20,22 @@ import scala.jdk.CollectionConverters._
 
 // Flow analysis (useFlow) is disabled
 class ConsistencyAnnotatedTypeFactory(checker: BaseTypeChecker) extends BaseAnnotatedTypeFactory(checker, false) {
+    shouldCache = true
     // force initialization of dependant classes (TreeAnnotator, TypeAnnotator, TypeHierarchy, etc.)
     this.postInit()
-    // disable caching for the annotated type factory, so that we can do multiple runs on the same
-    // compilation unit with different targets for @ThisConsistent
-    // TODO: see if we can instead manually clear all caches when reanalysing a compilation unit
-    shouldCache = true
 
+    /**
+     * Cache used for resolving @ThisConsistent correctly for multi-consistency class processing.
+     */
     private val methodTreeCache: mutable.Stack[mutable.Set[MethodTree]] = mutable.Stack.empty
+
+    /**
+     * Cache used for resolving @ThisConsistent correctly for multi-consistency class processing.
+     */
     private val methodInvocationTreeCache: mutable.Stack[mutable.Set[MethodInvocationTree]] = mutable.Stack.empty
-    pushNewCache() // populate cache for pre-class-visit operations
+
+    // populate cache for pre-class-visit operations
+    pushNewCache()
 
     var mixedInferenceVisitor: MixedInferenceVisitor = new MixedInferenceVisitor()(this)
 
@@ -57,6 +63,7 @@ class ConsistencyAnnotatedTypeFactory(checker: BaseTypeChecker) extends BaseAnno
      * Resolves @ThisConsistent for local variable declarations.
      */
     override def getAnnotatedTypeLhs(lhsTree: Tree): AnnotatedTypeMirror = {
+        // We don't need cache handling here, because local variables are never cached by the framework.
         val result = super.getAnnotatedTypeLhs(lhsTree)
         val element = TreeUtils.elementFromTree(lhsTree)
         element.getKind match {
@@ -69,32 +76,57 @@ class ConsistencyAnnotatedTypeFactory(checker: BaseTypeChecker) extends BaseAnno
 
     /**
      * @inheritdoc
-     * Resolves @ThisConsistent for method invocations (in return types, parameters)
+     * Resolves @ThisConsistent for method invocations (in return types, parameters).
      */
     override def methodFromUse(tree: MethodInvocationTree): AnnotatedTypeFactory.ParameterizedExecutableType = {
-        val useCache = methodInvocationTreeCache.top.contains(tree)
-        if (!useCache) {
-            methodInvocationTreeCache.top.add(tree)
-        }
-
-        withThisConsistentContext(tree, useCache) {
+        // TODO: figure out what effects caching has here
+        withThisConsistentContext(tree, useCache = false) {
             val typ = super.methodFromUse(tree)
             replaceThisConsistent(typ.executableType)
             typ
         }
     }
 
-    override def getAnnotatedType(tree: Tree): AnnotatedTypeMirror = tree match {
-        case methodTree: MethodTree =>
-            val useCache = methodTreeCache.top.contains(methodTree)
-            if (!useCache) {
-                methodTreeCache.top.add(methodTree)
-            }
-            withCache(useCache) {
+    /**
+     * @inheritdoc
+     * Handles caching for resolving @ThisConsistent correctly.
+     */
+    override def getAnnotatedType(tree: Tree): AnnotatedTypeMirror = {
+        // read and write cache are both controlled by 'useCache' in getAnnotatedType:
+        //
+        // super.getAnnotatedType = {
+        //     readCache (and return)          // need to disable this if we see this tree for the first time in
+        //     ...                             //  this class-visit-context
+        //     addComputedTypeAnnotations()    // we re-enable the cache (useCache = true) here in our override
+        //     ...
+        //     writeCache                      // always need to write cache here
+        // }
+
+        tree match {
+            case methodTree: MethodTree =>
+                // Keep track of which methods we have already seen for the current class-visit-context. If we see a
+                // new method, we have to disable the read cache to not reuse a wrong @ThisConsistent substitution
+                val useCache = methodTreeCache.top.contains(methodTree)
+                if (!useCache) {
+                    methodTreeCache.top.add(methodTree)
+                }
+                withCache(useCache) {
+                    // super method calls addComputedTypeAnnotations (see override)
+                    super.getAnnotatedType(tree)
+                }
+            case invocation: MethodInvocationTree =>
+                val useCache = methodInvocationTreeCache.top.contains(invocation)
+                if (!useCache) {
+                    methodInvocationTreeCache.top.add(invocation)
+                }
+                withCache(useCache) {
+                    // super method calls addComputedTypeAnnotations (see override)
+                    super.getAnnotatedType(tree)
+                }
+            case _ =>
+                // super method calls addComputedTypeAnnotations (see override)
                 super.getAnnotatedType(tree)
-            }
-        case _ =>
-            super.getAnnotatedType(tree)
+        }
     }
 
     /**
@@ -103,23 +135,26 @@ class ConsistencyAnnotatedTypeFactory(checker: BaseTypeChecker) extends BaseAnno
      * tree and type visitors.
      */
     override def addComputedTypeAnnotations(tree: Tree, typ: AnnotatedTypeMirror, iUseFlow: Boolean): Unit = {
+        // since we might have disabled the cache for reading in this.getAnnotatedType, we must enable it here again to
+        // allow saving the result to the cache in super.getAnnotatedType after this method returns
+        shouldCache = true
+
         tree match {
             case invocation: MethodInvocationTree =>
-                val useCache = methodInvocationTreeCache.top.contains(invocation)
-                if (!useCache) {
-                    methodInvocationTreeCache.top.add(invocation)
-                }
-                // the type of an invocation is the return type, which depends on the receiver object
-                // for other types of trees, the context is set by the consistency visitor when entering a method tree
-                withThisConsistentContext(invocation, useCache) {
+                // The type of an invocation is the return type, which depends on the receiver object.
+                // For other types of trees, the context is set by the consistency visitor when entering a method tree.
+                withThisConsistentContext(invocation, useCache = true) {
+                    // super method calls ConsistencyTreeAnnotator, ConsistencyTypeAnnotator
                     super.addComputedTypeAnnotations(tree, typ, iUseFlow)
                 }
             case _ =>
+                // super method calls ConsistencyTreeAnnotator, ConsistencyTypeAnnotator
                 super.addComputedTypeAnnotations(tree, typ, iUseFlow)
         }
     }
 
     override def addComputedTypeAnnotations(elt: Element, typ: AnnotatedTypeMirror): Unit = {
+        // no cache handling needed here, since getAnnotatedType for elements does not use caching
         // TODO: same as above, but we don't know the receiver, so the context must be set manually
         super.addComputedTypeAnnotations(elt, typ)
     }
@@ -169,7 +204,13 @@ class ConsistencyAnnotatedTypeFactory(checker: BaseTypeChecker) extends BaseAnno
     // ### @ThisConsistent helpers
     // #################################################################################################################
 
-    def withCache[R](useCache: Boolean)(f: => R): R = {
+    /**
+     * Executes a given function with either enabled or disabled checker-framework caching.
+     * @param useCache whether the checker-framework caching mechanism should be enabled or disabled
+     * @param f the function to execute
+     * @return the result of the executed function
+     */
+    private def withCache[R](useCache: Boolean)(f: => R): R = {
         val oldShouldCache = shouldCache
         shouldCache = useCache
         val result = f
@@ -183,12 +224,13 @@ class ConsistencyAnnotatedTypeFactory(checker: BaseTypeChecker) extends BaseAnno
      * @param f the function to execute
      * @return the result of the executed function
      */
-    def withThisConsistentContext[R](context: MethodInvocationTree, useCache: Boolean = false)(f: => R): R = withCache(useCache) {
-        thisConsistentContext.push(inferThisConsistentContext(context)(this))
-        val result = f
-        thisConsistentContext.pop()
-        result
-    }
+    def withThisConsistentContext[R](context: MethodInvocationTree, useCache: Boolean = false)(f: => R): R =
+        withCache(useCache) {
+            thisConsistentContext.push(inferThisConsistentContext(context)(this))
+            val result = f
+            thisConsistentContext.pop()
+            result
+        }
 
     /**
      * Replaces all occurrences of @ThisConsistent in the given type if any are present. If the type is a
