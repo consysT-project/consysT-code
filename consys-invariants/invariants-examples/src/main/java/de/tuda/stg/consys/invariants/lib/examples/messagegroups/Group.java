5df9e950ea733adc7358a19c791ae9fee92c67c6
George Zakhour <geezee@users.noreply.github.com>
2024-08-08 17:25:30 +0200

    Group implementation

--- consys-invariants/invariants-examples/src/main/java/de/tuda/stg/consys/invariants/lib/examples/messagegroups/Group.java
+++ consys-invariants/invariants-examples/src/main/java/de/tuda/stg/consys/invariants/lib/examples/messagegroups/Group.java
@@ -3,29 +3,29 @@ package de.tuda.stg.consys.invariants.lib.examples.messagegroups;
 
 import de.tuda.stg.consys.Mergeable;
 import de.tuda.stg.consys.annotations.invariants.ReplicatedModel;
-import static de.tuda.stg.consys.invariants.utils.InvariantUtils.numOfReplicas;
+import static de.tuda.stg.consys.invariants.utils.InvariantUtils.stateful;
 import static de.tuda.stg.consys.invariants.utils.InvariantUtils.replicaId;
-
-import de.tuda.stg.consys.annotations.invariants.ArrayUtils;
+import static de.tuda.stg.consys.invariants.utils.InvariantUtils.numOfReplicas;
 
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.HashSet;
 
-import static de.tuda.stg.consys.invariants.utils.InvariantUtils.stateful;
 
 @ReplicatedModel public class Group implements Mergeable<Group>, Serializable {
 
     public int[] counter;
     public Map<Integer, Set<String>> inboxes;
 
+    static { stateful(null); }
+
     //@ public invariant (\sum int i; i >= 0 && i < numOfReplicas(); counter[i]) >= 0;
     //@ public invariant (\sum int i; i >= 0 && i < numOfReplicas(); counter[i]) <= 3;
     //@ public invariant (\forall int uid; uid >= 5; inboxes.get(uid).isEmpty());
 
-    //@ public invariant (\sum int uid; uid >= 0 && uid < 5; inboxes.get(uid).isEmpty() ? 0 : 1) == (\sum int i; i >= 0 && i < numOfReplicas(); counter[i]);
+    //@ public invariant getCounterValue() == getUserCount();
 
     //@ ensures (\forall int i; ; counter[i] == 0);
     //@ ensures (\forall int uid; ; inboxes.get(uid).isEmpty());
@@ -34,14 +34,42 @@ import static de.tuda.stg.consys.invariants.utils.InvariantUtils.stateful;
         this.inboxes = new HashMap<>();
     }
 
+    //@ ensures \result == (\sum int i; i >= 0 && i < numOfReplicas(); counter[i]);
+    public int getCounterValue() {
+        int result = 0;
+        for (int i=0; i<numOfReplicas(); i++) result += counter[i];
+        return result;
+    }
+
+    //@ ensures \result == (\sum int uid; uid >= 0 && uid < 5; inboxes.get(uid).isEmpty() ? 0 : 1);
+    public int getUserCount() {
+        int result = 0;
+        for (Set<String> inbox : inboxes.values()) if (!inbox.isEmpty()) result++;
+        return result;
+    }
+
     //@ requires uid >= 0 && uid < 5;
-    //@ requires (\sum int i; i >= 0 && i < numOfReplicas(); counter[i]) < 3;
+    //@ requires getCounterValue() < 3;
     //@ ensures (!\old(inboxes).get(uid).isEmpty()) ? (counter == \old(counter)) : (counter[replicaId()] == \old(counter)[replicaId()] + 1);
     //@ ensures inboxes.get(uid).containsAll(inbox);
-    //@ ensures inboxes.get(uid).contains("welcome to the group");
+    //@ ensures inboxes.get(uid).contains("Welcome to the group!");
     //@ ensures (\forall int otherUser; ; inboxes.get(otherUser) == \old(inboxes).get(otherUser) || otherUser != uid);
     public Void addUser(Integer uid, Set<String> inbox) {
-        // TODO fill
+        if (uid < 0 || uid >= 5) {
+            throw new IllegalArgumentException("UserID is not in the expected range");
+        }
+        if (getCounterValue() >= 3) {
+            throw new RuntimeException("Cannot add user: group is already full");
+        }
+        if (inboxes.containsKey(uid)) {
+            inboxes.get(uid).addAll(inbox);
+        } else {
+            counter[replicaId()] += 1;
+            var newInbox = new HashSet<String>();
+            newInbox.add("Welcome to the group!");
+            newInbox.addAll(inbox);
+            inboxes.put(uid, newInbox);
+        }
         return null;
     }
 
@@ -51,11 +79,17 @@ import static de.tuda.stg.consys.invariants.utils.InvariantUtils.stateful;
     //@ ensures (\forall String msg; inboxes.get(uid).contains(msg); \old(inboxes).get(uid).contains(msg) || other.inboxes.get(uid).contains(msg));
     //@ ensures (\forall int otherUser; ; inboxes.get(otherUser) == \old(inboxes).get(otherUser) || otherUser == uid);
     public Void mergeUserInbox(Integer uid, Group other) {
+        if (other.inboxes.containsKey(uid)) {
+            var otherInbox = other.inboxes.get(uid);
+            if (inboxes.containsKey(uid)) inboxes.get(uid).addAll(otherInbox);
+            else inboxes.put(uid, otherInbox);
+        }
         return null;
     }
 
     //@ ensures (\forall int uid; !\old(inboxes).get(uid).isEmpty(); inboxes.get(uid).contains(message));
     public Void postMessage(String message) {
+        for (var inbox : inboxes.values()) if (!inbox.isEmpty()) inbox.add(message);
         return null;
     }
 
@@ -63,10 +97,12 @@ import static de.tuda.stg.consys.invariants.utils.InvariantUtils.stateful;
     //@ ensures (\forall int u; ; inboxes.get(u).containsAll(\old(inboxes).get(u)));
     //@ ensures (\forall int u; ; inboxes.get(u).containsAll(other.inboxes.get(u)));
     //@ ensures (\forall int u; ; (\forall String msg; inboxes.get(u).contains(msg); \old(inboxes).get(u).contains(msg) || other.inboxes.get(u).contains(msg)));
-    //@ ensures (\forall int u; ; stateful(mergeUserInbox(u, other)));
+    //@ ensures (\forall int u; !inboxes.get(u).isEmpty() ; stateful(mergeUserInbox(u, other)));
     //@ ensures (\forall int u; !other.inboxes.get(u).isEmpty(); stateful(addUser(u, other.inboxes.get(u))));
     public Void merge(Group other) {
-        // TODO fill
+        for (int i=0; i<numOfReplicas(); i++) counter[i] = Math.max(counter[i], other.counter[i]);
+        for (var user : inboxes.keySet()) mergeUserInbox(user, other);
+        for (var otherUser : other.inboxes.entrySet()) if (!otherUser.getValue().isEmpty()) addUser(otherUser.getKey(), otherUser.getValue());
         return null;
     }
 }
